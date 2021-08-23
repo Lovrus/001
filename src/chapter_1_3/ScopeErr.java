9d8f5365676a111f3e81968bccc5b2a83e0fa956
Lovrus <lovrus120581@gmail.com>
2021-08-23 23:52:34 +0300

    Программа с ошибкой.

--- src/chapter_1_3/ScopeErr.java
+++ src/chapter_1_3/ScopeErr.java
@@ -2,9 +2,9 @@ package chapter_1_3;
 
 public class ScopeErr {
     public static void main(String[] args) {
-        int bar = 1;
+       // int bar = 1;
         { // создается новая область действия
-          int bar = 2;  // Обишка во время комплияции -
+          //int bar = 2;  // Обишка во время комплияции -
             // переменная bar уже определена!
         }
     }
