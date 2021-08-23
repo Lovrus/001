62f0e4248d88af9a0ff512b2ca353b9dab2f6ea1
Lovrus <lovrus120581@gmail.com>
2021-08-24 00:01:39 +0300

    Приведение несовместимых типов.

--- src/chapter_1_3/Conversion.java
+++ src/chapter_1_3/Conversion.java
@@ -5,12 +5,16 @@ public class Conversion {
         byte b;
         int i = 257;
         double d = 323.142;
+
         System.out.println("Преобразование типа int в типа byte.");
         b = (byte) i;
+        System.out.println("i и b " + i + " " + b);
+
         System.out.println("Преобразование типа double в тип int.");
         i = (int) d;
         System.out.println("d и i " + d + " " + i);
-        System.out.println("Преобразование типа double в тип byte.");
+        System.out.println(
+                "Преобразование типа double в тип byte.");
         b = (byte) d;
         System.out.println("d и b " + d + " " + b);
     }
