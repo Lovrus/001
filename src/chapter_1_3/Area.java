0f66ea1b6882f9a3b75f9c97df7db1cc24cf31ec
Lovrus <lovrus120581@gmail.com>
2021-08-23 22:11:00 +0300

    Тип double

---
+++ src/chapter_1_3/Area.java
@@ -0,0 +1,12 @@
+package chapter_1_3;
+
+public class Area {
+    public static void main (String [] ards) {
+        double pi, r, a;
+
+        r = 10.8; // радиус окружности
+        pi = 3.1416; // приблизительное значение числа пи
+        a = pi * r * r; // вычислить площадь круга
+        System.out.println("Площадь круга равна" + a);
+    }
+}
