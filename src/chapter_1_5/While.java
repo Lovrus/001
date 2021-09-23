db9a6b124e297df70c0f1f433e06ac2b3ad290ab
Lovrus <lovrus120581@gmail.com>
2021-09-24 00:47:23 +0300

    Глава 5: wwitch, операторы цикла while, do-while

---
+++ src/chapter_1_5/While.java
@@ -0,0 +1,12 @@
+package chapter_1_5;
+
+// Продемонтстрировать применение оператора цикла while
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("такт " + n);
+            n--;
+        }
+    }
+}
