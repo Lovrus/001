3a0d0068bfe485c4c3fa823fbf0cf5b109f4a2a5
Lovrus <lovrus120581@gmail.com>
2021-09-29 00:27:24 +0300

    Глава 5, Прошел использование запятой в for, разновидность цикла for и  for each

--- src/chapter_1_5/Comma.java
+++ src/chapter_1_5/Comma.java
@@ -1,15 +1,13 @@
 package chapter_1_5;
 
-// Глава по использованию запятой
-public class Sample {
+// Использование запятой в операторе цикла for
+public class Comma {
     public static void main(String[] args) {
         int a, b;
 
-        b = 4;
-        for (a = 1; a < b; a++) {
+        for (a = 1, b = 4; a < b; a++, b--) {
             System.out.println("a = " + a);
             System.out.println("b = " + b);
-            b--;
         }
     }
 }
