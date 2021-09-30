4cd2266130f3195f81c67801f9f21182e79b944e
Lovrus <lovrus120581@gmail.com>
2021-09-30 07:22:43 +0300

    Глава 5 продолжаю for each

---
+++ src/chapter_1_5/NoChange.java
@@ -0,0 +1,20 @@
+package chapter_1_5;
+
+// Переменная цикла в стле for each доступна только для чтения
+public class NoChange {
+    public static void main(String[] args) {
+        int nums[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int x : nums) {
+            System.out.print(x + " ");
+            x = x * 10; // Этот оператор не оказывает никакого
+            // влияния на массив nums
+        }
+
+        System.out.println();
+
+        for (int x : nums)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+}
