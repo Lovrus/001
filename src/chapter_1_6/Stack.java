0a2c64cf20b5906048dc644ce37e0a08fc577021
Lovrus <lovrus120581@gmail.com>
2021-10-14 23:15:56 +0300

    Глава 6 Stack в код добавлен класс TestStack

--- src/chapter_1_6/Stack.java
+++ src/chapter_1_6/Stack.java
@@ -28,3 +28,23 @@ public class Stack {
             return stck[tos--];
     }
 }
+
+class TestStack {
+    public static void main(String[] args) {
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+
+        // разместить числа в стеке
+        for (int i = 0; i < 10; i++) mystack1.push(i);
+        for (int i = 10; i < 20; i++) mystack2.push(i);
+
+        // извлечь эти чимсла из стека
+        System.out.println("Содержимое стека mystack1:");
+        for (int i = 0; i < 10; i++)
+            System.out.println(mystack1.pop());
+
+        System.out.println("Содержимое стека mystack2:");
+        for (int i = 0; i < 10; i++)
+            System.out.println(mystack2.pop());
+    }
+}
