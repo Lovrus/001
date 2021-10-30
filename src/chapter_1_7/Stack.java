b4cf7dcecfdca54b985288c430c5e937d245ecde
Lovrus <lovrus120581@gmail.com>
2021-10-30 22:59:34 +0300

    Глава 7. Введение в управление доступом-2 продолжение.

--- src/chapter_1_7/Stack.java
+++ src/chapter_1_7/Stack.java
@@ -5,7 +5,7 @@ package chapter_1_7;
 public class Stack {
     /* Теперь переменные stck и tos являются закрытыми.
     Это означает, что они не могут быть случайно или
-    намеренно изменены таких образом, чтобы нарушить стек
+/    намеренно изменены таких образом, чтобы нарушить стек*+\
      */
     private int stck[] = new int[10];
     private int tos;
@@ -30,5 +30,30 @@ public class Stack {
             return 0;
         } else
             return stck[tos--];
+    }
+}
+
+class TestStack {
+    public static void main(String[] args) {
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+
+        // разместить числа в стеке
+        for (int i = 0; i < 0; i++) mystack1.push(i);
+        for (int i = 10; i < 20; i++) mystack2.push(i);
+
+        // извелчь этичисла из стека
+        System.out.println("Стек в mystack1:");
+        for (int i = 0; i < 10; i++)
+            System.out.println(mystack1.pop());
+
+        System.out.println("Стек в mystack2:");
+
+        for (int i = 0; i < 10; i++)
+            System.out.println(mystack2.pop());
+
+        // эти операторы недопустимы
+        // mystack1.tos = - 2;
+        // mystack2.stck[3] = 100;
     }
 }
