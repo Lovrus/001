8b9aa2bcd71c68cc1098e7be9dec84fb8ba35b3b
Lovrus <lovrus120581@gmail.com>
2021-11-15 23:38:44 +0300

    2 Урок Алишева. Пока я дочитываю 7 главу книги.

--- src/Alishev/HelloWorld.java
+++ src/Alishev/HelloWorld.java
@@ -1,5 +1,6 @@
 package Alishev;
 
+// Урок 1. Hello World
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/Alishev/Variables.java
@@ -0,0 +1,20 @@
+package Alishev;
+
+// Урок 2. Переменные. Примитивные типы данных.
+public class Variables {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 557;
+        short myShort = 3266;
+        long myLong = 26246246;
+
+        double myDouble = 235.35;
+        float myFloat = 2362.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; // -128-128
+        System.out.println(myInt);
+    }
+}
