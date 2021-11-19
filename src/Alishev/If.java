a3f468b2f3750ebc9edb64f2eb865a65e6d555d8
Lovrus <lovrus120581@gmail.com>
2021-11-19 23:01:29 +0300

    Урок 6. Условный оператор if

---
+++ src/Alishev/If.java
@@ -0,0 +1,12 @@
+package Alishev;
+// Урок 6. Условный оператор if
+public class If {
+    public static void main(String[] args) {
+        int myInt = 5;
+        if (myInt < 10) {
+            System.out.println("да, верно.");
+        } else if (myInt < 20) {
+            System.out.println("нет, неверно");
+        }
+    }
+}
