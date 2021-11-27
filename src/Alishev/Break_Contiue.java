21aa46fae81efa2810b071089954dd9b097826f2
Lovrus <lovrus120581@gmail.com>
2021-11-27 22:17:19 +0300

    Урок 9.Операторы break и continue

---
+++ src/Alishev/Break_Contiue.java
@@ -0,0 +1,13 @@
+package Alishev;
+// Урок 9.Операторы break и continue
+
+class Break_Continue {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("Это нечетное число" + i);
+        }
+    }
+}
