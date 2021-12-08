b96a00112bf3bdc18e6fd573f530e855567ad470
Lovrus <lovrus120581@gmail.com>
2021-12-09 00:17:09 +0300

    Глава 8. Применение абстрактных классов простой пример.

---
+++ src/chapter_1_8/A9.java
@@ -0,0 +1,29 @@
+package chapter_1_8;
+
+// Простой пример абстракции
+abstract class A9 {
+    void callme9() {
+
+    }
+
+    // абстрактные классы вс же могут
+    //содержать конкретные методы
+    void callmetoo() {
+        System.out.println("Это конкретный метод.");
+    }
+}
+
+class B9 extends A9 {
+    void callme9() {
+        System.out.println(
+                "Реализация метода callme() в классе В.");
+    }
+}
+
+class AbstraktDemo {
+    public static void main(String[] args) {
+        B9 b = new B9();
+        b.callme9();
+        b.callmetoo();
+    }
+}
