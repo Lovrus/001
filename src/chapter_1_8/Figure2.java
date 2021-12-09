648f14b52f474d8f55b65961b942c06f035d77eb
Lovrus <lovrus120581@gmail.com>
2021-12-10 00:15:41 +0300

    Глава 8. Применение абстрактных классов основной пример.

---
+++ src/chapter_1_8/Figure2.java
@@ -0,0 +1,55 @@
+package chapter_1_8;
+
+// Применение абстрактных методов и классов
+abstract class Figure2 {
+    double dim1;
+    double dim2;
+
+    Figure2(double a, double b) {
+        dim1 = a;
+        dim2 = b;
+    }
+
+    // теерь метод area() объявляется абстратным
+    abstract double area();
+}
+
+class Rectangle2 extends Figure2 {
+    Rectangle2(double a, double b) {
+        super(a, b);
+    }
+
+    // переопределить метод area() для четырехугольника
+    double area() {
+        System.out.println("В области четырехугольника.");
+        return dim1 * dim2;
+    }
+}
+
+class Triangle2 extends Figure2 {
+    Triangle2(double a, double b) {
+        super(a, b);
+    }
+
+    // переопределить метод area() для
+    // прямоугльного треугольника
+    double area() {
+        System.out.println("В области треугольника.");
+        return dim1 * dim2 / 2;
+    }
+}
+
+class AbstractAreas2 {
+    public static void main(String[] args) {
+        // Figure2 f = new Figwre2(10,10); // теперь недоступно
+        Rectangle2 r = new Rectangle2(9, 5);
+        Triangle2 t = new Triangle2(10, 8);
+        Figure2 figref; // верно, хотя объект не создается
+
+        figref = r;
+        System.out.println("Площадь равна " + figref.area());
+
+        figref = t;
+        System.out.println("Площадь равна " + figref.area());
+    }
+}
