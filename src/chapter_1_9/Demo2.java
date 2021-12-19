fc16d6ececa40e699d3eacbaaad8baf75c90b735
Lovrus <lovrus120581@gmail.com>
2021-12-19 23:00:55 +0300

    Глава 9. Пример доступа к пакетам - Демонстрационный пакет р2

---
+++ src/chapter_1_9/Demo2.java
@@ -0,0 +1,10 @@
+package chapter_1_9;
+// Демонстрационный пакет р2
+
+// получить экземпляры различных классов из пакета р2
+public class Demo2 {
+    public static void main(String[] args) {
+        Protection2 ob1 = new Protection2();
+        OtherPackage ob2 = new OtherPackage();
+    }
+}
