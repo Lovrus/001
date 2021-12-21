e2c8113dd164ce9427c7f714c720bafd1b185b75
Lovrus <lovrus120581@gmail.com>
2021-12-21 23:01:42 +0300

    Импорт пакетов - продолжение

---
+++ src/chapter_1_9/TestBalance.java
@@ -0,0 +1,11 @@
+package chapter_1_9;
+
+public class TestBalance {
+    public static void main(String[] args) {
+        /* Класс Balance объявлен как public, поэтому им можно
+        воспользоваться и вызвать его конструктор */
+        Balance2 test = new Balance2("J. J. Jaspers", 99.98);
+
+        test.show(); // можно так же вызвать метод show()
+    }
+}
