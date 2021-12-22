c210288b3368a74ab83ca15c3f64600e378ba45e
Lovrus <lovrus120581@gmail.com>
2021-12-22 23:25:40 +0300

    Реализация интерфейсов

---
+++ src/chapter_1_9/Client.java
@@ -0,0 +1,13 @@
+package chapter_1_9;
+
+interface Callback {
+    void callback(int param);
+}
+
+public class Client implements Callback {
+    // реализовать интерфейс Callback
+    public void callback(int p) {
+        System.out.println("Метод callback(), "
+                + " вызываемый со значением " + p);
+    }
+}
