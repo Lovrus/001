631f1ae5bd0a67cdcc47b52c6084ae1ec052e97b
Lovrus <lovrus120581@gmail.com>
2021-12-26 15:53:22 +0300

    Применение интерфейсов

---
+++ src/chapter_1_9/FixedStack.java
@@ -0,0 +1,52 @@
+package chapter_1_9;
+
+interface IntStack {
+    void push(int item); // соранить элемент в стеке
+
+    int pop(); // извлечь элемент из стека
+}
+
+// Реализация интерфейса Intstack для стека
+// фиксированного размера
+public class FixedStack implements IntStack {
+    private int stck[];
+    private int tos;
+
+    // выделить память и иницилизировать стек
+    FixedStack(int size) {
+        stck = new int[size];
+        tos = -1;
+    }
+
+    // разместить элемент в стеке
+    public void push(int item) {
+        if (tos == stck.length - 1) // использовать поле длины стека
+            System.out.println("Стек заполен.");
+        else
+            stck[++tos] = item;
+    }
+
+    // извлечь элемент из стека
+    public int pop() {
+        if (tos < 0) {
+            System.out.println("Стек не загружен.");
+            return 0;
+        } else
+            return stck[tos--];
+    }
+}
+
+class IFTest {
+    public static void main(String[] args) {
+        FixedStack mystack1 = new FixedStack(5);
+        FixedStack mystack2 = new FixedStack(8);
+
+        // расместить числа в стеке
+        for (int i = 0; i < 5; i++) mystack1.push(i);
+        for (int i = 0; i < 8; i++) mystack2.push(i);
+
+        System.out.println("Стек в mystack2:");
+        for (int i = 0; i < 8; i++)
+            System.out.println(mystack2.pop());
+    }
+}
