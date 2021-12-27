68e6cef4fbb376bd1b8137763d14ce2b1c8e25b4
Lovrus <lovrus120581@gmail.com>
2021-12-28 00:15:49 +0300

    Урок 18. Инкапсуляция. Сеттеры и геттеры.

--- src/Alishev/ClassAndObjects2.java
+++ src/Alishev/ClassAndObjects2.java
@@ -1,15 +1,14 @@
 package Alishev;
 
-// Урок 17. Параметры метода.
+// Урок 18. Инкапсуляция. Сеттеры и геттеры.
 class ClassesAndObjects2 {
     public static void main(String[] args) {
         Person2 person1 = new Person2();
-        person1.setNameAndAge("Роман",20);
-        String s1 = "Вова";
-        Person2 person2 = new Person2();
-        person2.setNameAndAge(s1,30);
-        person1.speak();
-        person2.speak();
+        person1.setName("");
+        person1.setAge(-4);
+        System.out.println("Выводим значение в main методе: " + person1.getName());
+        System.out.println("Выводим значение в main методе: " + person1.getAge());
+
     }
 }
 
@@ -17,8 +16,32 @@ class Person2 {
     // У класса могут быть:
     //1. Данные (поля).
     //2. Действия, которые он может совершать (методы)
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName) {
+        if (userName.isEmpty()) {
+            System.out.println("Ты ввел пустое имя");
+        } else {
+            name = userName;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int userAge) {
+        if (userAge < 0) {
+            System.out.println("Возраст должен быть положительным");
+        } else {
+            age = userAge;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     void setNameAndAge(String username, int userage) {
         name = username;
