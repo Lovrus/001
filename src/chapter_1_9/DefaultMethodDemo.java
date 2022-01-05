3a204bc0b8cf80b464bd31b6941cedeecc356cdb
Lovrus <lovrus120581@gmail.com>
2022-01-06 01:11:14 +0300

    Основы применения методов с реализацией по умолчанию

---
+++ src/chapter_1_9/DefaultMethodDemo.java
@@ -0,0 +1,17 @@
+package chapter_1_9;
+// Использовать метод с реализцией по умолчанию
+public class DefaultMethodDemo {
+    public static void main(String[] args) {
+
+        MyIFImp obj = new MyIFImp();
+
+        // Метод getNumber() можно вызвать, так как он
+        // явно реализован в классе MyIFImp:
+        System.out.println(obj.getNumber());
+
+        // Метод getString() так же можно вызвать,
+        // так как в интерфейсе имеется его реализация
+        // по умолчанию:
+        System.out.println(obj.getString());
+    }
+}
