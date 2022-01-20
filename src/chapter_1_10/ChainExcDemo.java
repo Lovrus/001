f384e647002cfada23f6a98dcb8eaa3fdb8ea975
Lovrus <lovrus120581@gmail.com>
2022-01-21 00:01:42 +0300

    Цепочки исключений

---
+++ src/chapter_1_10/ChainExcDemo.java
@@ -0,0 +1,29 @@
+package chapter_1_10;
+
+// Продемонстрировать цепочки исключений
+public class ChainExcDemo {
+    static void demoproc() {
+
+        // создать исключение
+        NullPointerException e =
+                new NullPointerException("верхний уровень");
+
+        // добавить причину исключения
+        e.initCause(new ArithmeticException("причина"));
+
+        throw e;
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e) {
+            // вывести исключение верхнего уровня
+            System.out.println("Перехвачено исключение: " + e);
+
+            // вывести исключение, послужившее причиной
+            // для исключения верхнего уровня
+            System.out.println("Первопричина: " + e.getCause());
+        }
+    }
+}
