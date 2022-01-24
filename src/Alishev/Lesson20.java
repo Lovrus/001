eac46475423b542c7a01c53a9c4fa417d37bba11
Lovrus <lovrus120581@gmail.com>
2022-01-25 00:30:09 +0300

    Урок 21. Ключевое слово static

--- src/Alishev/Lesson20.java
+++ src/Alishev/Lesson20.java
@@ -1,9 +1,12 @@
 package Alishev;
 
-// Урок 20. Конструкторы
+// Урок 21. Ключевое слово static
 public class Lesson20 {
     public static void main(String[] args) {
-        Human20 human20 = new Human20("Bob", 15);
+       Human20.description20 = "Nice";
+       Human20.getDescripnion();
+       Human20 h1 = new Human20("Bob", 40);
+
     }
 
 }
@@ -12,17 +15,10 @@ class Human20 {
     private String name20;
     private int age20;
 
-    public Human20() {
-        this.name20 = "Имя по умолчанию";
-        this.age20 = 0;
-    }
+    public static String description20;
 
-    public Human20(String name20) {
-        System.out.println("Привет из второго конструктора!");
-        this.name20 = name20;
-    }
 
-    public Human20(String name20, int age20) {
+        public Human20(String name20, int age20) {
         System.out.println("Привет из третьего конструктора!");
         this.name20 = name20;
         this.age20 = age20;
@@ -34,5 +30,9 @@ class Human20 {
 
     public void setAge20(int age20) {
         this.age20 = age20;
+    }
+
+    public static void getDescripnion() {
+        System.out.println(description20);
     }
 }
