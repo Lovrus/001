8697398f7fbcd92b4e39dee54e92e1ca857d036d
Lovrus <lovrus120581@gmail.com>
2022-01-25 23:31:11 +0300

    Урок 21. Продолжение

--- src/Alishev/Lesson20.java
+++ src/Alishev/Lesson20.java
@@ -3,10 +3,15 @@ package Alishev;
 // Урок 21. Ключевое слово static
 public class Lesson20 {
     public static void main(String[] args) {
-       Human20.description20 = "Nice";
-       Human20.getDescripnion();
-       Human20 h1 = new Human20("Bob", 40);
 
+        Human20 h1 = new Human20("Bob", 40);
+        Human20 h2 = new Human20("Tom", 30);
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
+Human20 h3 = new Human20("Rob", 50);
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
+        h3.printNumberOfPeople();
     }
 
 }
@@ -14,14 +19,13 @@ public class Lesson20 {
 class Human20 {
     private String name20;
     private int age20;
+    private static int countPeople;
 
-    public static String description20;
 
-
-        public Human20(String name20, int age20) {
-        System.out.println("Привет из третьего конструктора!");
+    public Human20(String name20, int age20) {
         this.name20 = name20;
         this.age20 = age20;
+        countPeople++;
     }
 
     public void setName20(String name20) {
@@ -32,7 +36,7 @@ class Human20 {
         this.age20 = age20;
     }
 
-    public static void getDescripnion() {
-        System.out.println(description20);
+    public void printNumberOfPeople() {
+        System.out.println("Number of piople is " + countPeople);
     }
 }
