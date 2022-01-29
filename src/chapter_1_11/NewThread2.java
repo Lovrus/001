15654d13cfe7578291d4a72c97a00e7a90f83014
Lovrus <lovrus120581@gmail.com>
2022-01-29 12:52:52 +0300

    Расширение класса Thread

---
+++ src/chapter_1_11/NewThread2.java
@@ -0,0 +1,41 @@
+package chapter_1_11;
+
+// Создать второй поток исполнения, расширив клас Thread
+class NewThread3 extends Thread {
+    NewThread3() {
+        // создать новый поток исполнения
+        super("Демонстрациооный поток");
+        System.out.println("Дочерний поток: " + this);
+        start();// запустить поток на исполнение
+    }
+
+    // Точка входа во второй поток исполнения
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Дочерний поток " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Дочерний поток прерван.");
+        }
+        System.out.println("Дочерний поток завершен.");
+    }
+}
+
+class ExtendThread {
+    public static void main(String[] args) {
+        new NewThread3(); // создать новый поток исполнения
+
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Главный поток: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван.");
+        }
+
+        System.out.println("Главный поток завершен.");
+    }
+}
