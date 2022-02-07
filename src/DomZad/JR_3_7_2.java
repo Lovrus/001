60d6d4df7de2ad4725bfabe40ec5ba6b8068ee63
Lovrus <lovrus120581@gmail.com>
2022-02-08 00:43:57 +0300

    JR_3_7_2

---
+++ src/DomZad/JR_3_7_2.java
@@ -0,0 +1,17 @@
+package DomZad;
+
+public class JR_3_7_2 {
+    public static void main(String[] args) {
+        checkInterval(60);
+        checkInterval(112);
+        checkInterval(10);
+    }
+
+    public static void checkInterval(int a) {
+        if (a >= 50 && a <= 99) {
+            System.out.println("Число " + a + " содержится в интервале.");
+        } else {
+            System.out.println("Число " + a + " не содержится в интервале.");
+        }
+    }
+}
