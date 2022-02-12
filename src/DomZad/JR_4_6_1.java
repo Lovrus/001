31886acd6c2922605d7de02b8b876ddd72155a54
Lovrus <lovrus120581@gmail.com>
2022-02-12 13:41:03 +0300

    JR_4_6_1

---
+++ src/DomZad/JR_4_6_1.java
@@ -0,0 +1,12 @@
+package DomZad;
+
+public class JR_4_6_1 {
+    public static void main (String[] args) {
+        for (int a = 1; a <=10; a++) {
+            for (int b = 1; b <=10; b++) {
+                System.out.print(a*b + " ");
+            }
+            System.out.println();
+        }
+    }
+}
