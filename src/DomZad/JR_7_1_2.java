bf5801a553b8e6edaf7673807f664cbde7eed857
Lovrus <lovrus120581@gmail.com>
2022-02-21 23:35:58 +0300

    JR_7_

---
+++ src/DomZad/JR_7_1_2.java
@@ -0,0 +1,15 @@
+package DomZad;
+
+/*
+Упорядоченная информация
+*/
+public class JR_7_1_2 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.println("От меня вам всем приветик.");
+    }
+}
+
+
