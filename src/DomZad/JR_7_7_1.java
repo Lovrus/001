29d38873dedd05cadf076f3b3dcf5c3bfa64590f
Lovrus <lovrus120581@gmail.com>
2022-03-01 20:02:22 +0300

    JR_7_7_1

---
+++ src/DomZad/JR_7_7_1.java
@@ -0,0 +1,17 @@
+package DomZad;
+/*
+Вспоминаем геометрию
+*/
+public class JR_7_7_1 {
+
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Плошадь круга равна: " + pi * radius * radius);
+        System.out.println("Периметр круга равен: " + pi * diameter);
+    }
+}
+
+
--- src/DomZad/Zadanie_1.java
+++ src/DomZad/Zadanie_1.java
@@ -1,7 +1,25 @@
 package DomZad;
 
-public class Zadanie_1 {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
+class Parser {
+    public Parser() {
+    }
+
+    public static void main(String[] var0) {
+        String[] var1 = var0;
+        int var2 = var0.length;
+
+        for (int var3 = 0; var3 < var2; ++var3) {
+            String var4 = var1[var3];
+
+            try {
+                String[] var5 = var4.split("=");
+                if (var5[0].equals("--param")) {
+                    System.out.println("Have `--param` with value: " + var5[1]);
+                }
+            } catch (ArrayIndexOutOfBoundsException var6) {
+                System.out.println("'" + var4 + "' in not valid '--key=value' argument");
+            }
+        }
+
     }
 }
