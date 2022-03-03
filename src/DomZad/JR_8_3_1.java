9ef50214e32cddc3468d10f7b82cac761f6808a5
Lovrus <lovrus120581@gmail.com>
2022-03-03 22:17:14 +0300

    JR_8_3_3

---
+++ src/DomZad/JR_8_3_1.java
@@ -0,0 +1,18 @@
+package DomZad;
+/*
+Коты - это хорошо
+*/
+public class JR_8_3_1 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat =new Cat();
+        Cat dog =new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
+
+
---
+++ src/DomZad/JR_8_3_3.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Три собаки - это сила
+*/
+public class JR_8_3_3 {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.name = "Max";
+
+        //напишите тут ваш код
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
+    }
+
+    public static class Dog {
+        public String name;
+    }
+}
