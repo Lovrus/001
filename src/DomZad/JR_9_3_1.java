544e19adcd084036215be48db9d16e5ba8cb1650
Lovrus <lovrus120581@gmail.com>
2022-03-09 22:57:09 +0300

    JR_9_3_3

---
+++ src/DomZad/JR_9_3_1.java
@@ -0,0 +1,13 @@
+package DomZad;
+/*
+Экранирование символов
+*/
+public class JR_9_3_1 {
+
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
+
+
