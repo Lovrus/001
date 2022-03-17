19c1c3d3832ca26805aa7e5506782658db2d6094
Lovrus <lovrus120581@gmail.com>
2022-03-17 22:01:04 +0300

    JR_9_7_2

---
+++ src/DomZad/JR_9_7_2.java
@@ -0,0 +1,19 @@
+package DomZad;
+/*
+Разворот строки
+*/
+public class JR_9_7_2 {
+
+    public static void main(String[] args) {
+        String string = "Ходит кот задом наперед";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.reverse().toString();
+    }
+}
+
+
