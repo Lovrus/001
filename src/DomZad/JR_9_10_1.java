390d77c4189b3fd468df2ecea5a89875b8fe9ca4
Lovrus <lovrus120581@gmail.com>
2022-03-18 22:29:25 +0300

    JR_9_10_1

---
+++ src/DomZad/JR_9_10_1.java
@@ -0,0 +1,19 @@
+package DomZad;
+/*
+Литералы
+*/
+public class JR_9_10_1 {
+
+    public byte b = 0;
+    public short s = 123;
+    public int i = 1111;
+    public long l = 2222L;
+    public float f = 34.2f;
+    public double d = 653.89d;
+    public char c = 'J';
+    public boolean boo = true;
+
+    public String string = "Hello";
+}
+
+
