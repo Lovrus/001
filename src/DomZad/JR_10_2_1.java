804ebd9c8671189402c4a10b9ea531f03df40f39
Lovrus <lovrus120581@gmail.com>
2022-03-19 23:02:00 +0300

    JR_10_2_1

---
+++ src/DomZad/JR_10_2_1.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Кошачья бойня(1)
+*/
+public class JR_10_2_1 {
+
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
