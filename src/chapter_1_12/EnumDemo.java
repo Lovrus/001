3cae88f4eae892cbfdaf4e25ecdd31eb2cb67df3
Lovrus <lovrus120581@gmail.com>
2022-03-21 21:49:54 +0300

    EnumDemo

---
+++ src/chapter_1_12/EnumDemo.java
@@ -0,0 +1,44 @@
+package chapter_1_12;
+
+enum Apple {
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+}
+
+public class EnumDemo {
+    public static void main(String[] args) {
+        {
+            Apple ap;
+            ap = Apple.RedDel;
+
+            // вывести значение перечислимого типа
+            System.out.println("Значение ар: " + ap);
+            System.out.println();
+
+            ap = Apple.GoldenDel;
+
+            // сравнить два значения перечислимого типа
+            if (ap == Apple.GoldenDel)
+                System.out.println(
+                        "Переменная ар содержит GoldenDel.\n");
+            // приенимть перечиление для управления
+            // оператором switch
+            switch (ap) {
+                case Jonathan:
+                    System.out.println("Сорт Jonathan красный.");
+                    break;
+                case GoldenDel:
+                    System.out.println("Сорт GoldenDel Delicious желтый.");
+                    break;
+                case RedDel:
+                    System.out.println("Сорт Red Delicious красный.");
+                    break;
+                case Winesap:
+                    System.out.println("Сорт Winesap красный.");
+                    break;
+                case Cortland:
+                    System.out.println("Сорт Cortland красный.");
+                    break;
+            }
+        }
+    }
+}
