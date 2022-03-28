e596dba61fe5a2a39b06c93d6ff91f375685c427
Lovrus <lovrus120581@gmail.com>
2022-03-28 22:32:46 +0300

    JR_10_4_1

---
+++ src/DomZad/JR_10_4_1.java
@@ -0,0 +1,32 @@
+package DomZad;
+/*
+Заполнить класс Friend
+*/
+public class JR_10_4_1 {
+
+    public String name;
+    public int age;
+    public char sex;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void initialize(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
