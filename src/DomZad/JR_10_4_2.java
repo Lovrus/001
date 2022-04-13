ac7933a7a2f8ecb0afda520dbdc12f25b908d210
Lovrus <lovrus120581@gmail.com>
2022-04-13 22:57:12 +0300

    Marker

---
+++ src/DomZad/JR_10_4_2.java
@@ -0,0 +1,29 @@
+package DomZad;
+/*
+Заполнить класс Dog
+*/
+
+public class JR_10_4_2 {
+    public String name;
+    public int height;
+    public String color;
+
+    public void initialize(String name) {
+        this.name = name;
+    }
+
+    public void initialize(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public void initialize(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
