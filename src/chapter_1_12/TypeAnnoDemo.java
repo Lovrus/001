87e9eb6209c350e0bb7b28176fed5a0a8c7a56ce
Lovrus <lovrus120581@gmail.com>
2022-04-15 22:57:52 +0300

    TypeAnnoDemo

---
+++ src/chapter_1_12/TypeAnnoDemo.java
@@ -0,0 +1,9 @@
+package chapter_1_12;
+// Продемонстрировать применение нескольких
+// типовых аннотаций
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+public interface TypeAnnoDemo {
+}
