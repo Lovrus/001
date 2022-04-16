6e8a2e24cef03d34a3156d8b59f5225ffa8f5ffd
Lovrus <lovrus120581@gmail.com>
2022-04-16 22:53:08 +0300

    TypeAnnoDemo

--- src/chapter_1_12/TypeAnnoDemo.java
+++ src/chapter_1_12/TypeAnnoDemo.java
@@ -5,5 +5,16 @@ package chapter_1_12;
 import java.lang.annotation.*;
 import java.lang.reflect.*;
 
+// Аннотация-маркер, которую можно применить к типу данных
+@Target(ElementType.TYPE_USE)
+@interface TypeAnno {
+}
+
+// Еще одна аннотация-маркер, которую можно
+// применить к типу данных
+@Target(ElementType.TYPE_USE)
+@interface NotZeroLen {
+}
+
 public interface TypeAnnoDemo {
 }
