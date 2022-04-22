fabc2fe61f0b36bad53a12b1887800363bba9da1
Lovrus <lovrus120581@gmail.com>
2022-04-22 22:53:53 +0300

    JR_10_4_7

---
+++ src/DomZad/JR_10_4_7.java
@@ -0,0 +1,27 @@
+package DomZad;
+
+/*
+Инициализация объектов
+*/
+public class JR_10_4_7 {
+    String name;
+    char sex;
+    int money;
+    int weight;
+    double size;
+
+    public void initialize(String name, int money, char sex, int weight,
+                           double size) {
+        this.name = name;
+        this.money = money;
+        this.sex = sex;
+        this.weight = weight;
+        this.size = size;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
