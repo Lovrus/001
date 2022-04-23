a2d6e66bcafb202732dfa771d1e1cd9372b8e2d2
Lovrus <lovrus120581@gmail.com>
2022-04-23 23:24:49 +0300

    JR_10_6_1

---
+++ src/DomZad/JR_10_6_1.java
@@ -0,0 +1,30 @@
+package DomZad;
+
+/*
+Друзей не купишь
+*/
+public class JR_10_6_1 {
+    public String name;
+    public int age;
+    public char sex;
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public Friend(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
