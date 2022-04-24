ab01702da3c5d58f70ec2c82da56ef14cabfe371
Lovrus <lovrus120581@gmail.com>
2022-04-24 23:29:12 +0300

    JR_10_6_2

---
+++ src/DomZad/JR_10_6_2.java
@@ -0,0 +1,23 @@
+package DomZad;
+
+/*
+Основа колеса
+*/
+public class JR_10_6_2 {
+
+    public double x;
+    public double y;
+    public double r;
+
+    //напишите тут ваш код
+    public JR_10_6_2(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
