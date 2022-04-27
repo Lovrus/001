b7fa5b93ff8fc29581df980fbea313dfd6ac3fa2
Lovrus <lovrus120581@gmail.com>
2022-04-27 22:24:34 +0300

    JR_10_6_7

---
+++ src/DomZad/JR_10_6_7.java
@@ -0,0 +1,33 @@
+package DomZad;
+
+/*
+Максимум конструкторов
+*/
+public class JR_10_6_7 {
+
+    public double x;
+    public double y;
+    public double radius;
+
+    public JR_10_6_7() {
+    }
+
+    public JR_10_6_7(double x) {
+        this.x = x;
+    }
+
+    public JR_10_6_7(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public JR_10_6_7(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
