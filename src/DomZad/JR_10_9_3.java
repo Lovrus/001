05e1a1a1404a9f852f416ff5173a7a0214c8be78
Lovrus <lovrus120581@gmail.com>
2022-04-30 23:51:17 +0300

    JR_10_9_3

---
+++ src/DomZad/JR_10_9_3.java
@@ -0,0 +1,30 @@
+package DomZad;
+
+/*
+Регистрируем собачек
+*/
+public class JR_10_9_3 {
+
+    public String name;
+    public int height;
+    public String color;
+
+    public JR_10_9_3(String name) {
+        this.name = name;
+    }
+
+    public JR_10_9_3(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public JR_10_9_3(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
