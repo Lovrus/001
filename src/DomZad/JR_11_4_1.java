a4853b1767df555db56b70d747241ad0beb29051
Lovrus <lovrus120581@gmail.com>
2022-05-06 23:00:19 +0300

    JR_11_4_1

---
+++ src/DomZad/JR_11_4_1.java
@@ -0,0 +1,17 @@
+package DomZad;
+/*
+Подсчет котов
+*/
+public class JR_11_4_1 {
+
+    private static int catCount = 0;
+
+    public JR_11_4_1() {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
