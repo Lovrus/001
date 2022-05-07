2bc6707b30d11a778de2906c7415735a5a7c43c4
Lovrus <lovrus120581@gmail.com>
2022-05-07 23:21:26 +0300

    JR_11_4_2

---
+++ src/DomZad/JR_11_4_2.java
@@ -0,0 +1,22 @@
+package DomZad;
+
+/*
+Статические методы: int getCatCount() и setCatCount(int)
+*/
+public class JR_11_4_2 {
+
+    private static int catCount = 0;
+
+    public static int getCatCount() {
+        return catCount;
+    }
+
+    public static void setCatCount(int catCount) {
+        JR_11_4_2.catCount = catCount;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
