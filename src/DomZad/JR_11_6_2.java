29cfaa27df2a64f7cdd4a4d7621adc8abc5a5de0
Lovrus <lovrus120581@gmail.com>
2022-05-12 22:30:32 +0300

    JR_11_6_2

---
+++ src/DomZad/JR_11_6_2.java
@@ -0,0 +1,16 @@
+package DomZad;
+
+import java.util.Scanner;
+
+/*
+Все буквы маленькие
+*/
+
+public class JR_11_6_2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        line = line.toLowerCase();
+        System.out.println(line);
+    }
+}
