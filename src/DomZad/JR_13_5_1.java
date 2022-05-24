60e1c7da62ee3d104de6d0201d98304bea6b9803
Lovrus <lovrus120581@gmail.com>
2022-05-24 22:26:25 +0300

    JR_13_5_1

---
+++ src/DomZad/JR_13_5_1.java
@@ -0,0 +1,29 @@
+package DomZad;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Слова в обратном порядке
+*/
+
+public class JR_13_5_1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String string = reader.readLine();
+            list.add(string);
+        }
+
+        list.remove(2);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            String string = list.get(i);
+            System.out.println(string);
+        }
+    }
+}
+
