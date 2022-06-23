631c6603ab42b4ba0e6388cc9866b867ddb33313
Lovrus <lovrus120581@gmail.com>
2022-06-24 00:22:53 +0300

    JR_14_5_1

---
+++ src/DomZad/JR_14_5_1.java
@@ -0,0 +1,23 @@
+package DomZad;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*
+LinkedList и ArrayList
+*/
+
+public class JR_14_5_1 {
+    public static Object createArrayList() {
+        return new ArrayList();
+    }
+
+    public static Object createLinkedList() {
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
