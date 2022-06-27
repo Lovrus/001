1f70431eae20f5c1715a3f5f341857bb9ab38d2d
Lovrus <lovrus120581@gmail.com>
2022-06-27 22:23:47 +0300

    JR_14_5_4

---
+++ src/DomZad/JR_14_5_4.java
@@ -0,0 +1,31 @@
+package DomZad;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+Квартет «Методы»
+*/
+
+public class JR_14_5_4 {
+    public static List getListForGet() {
+        return new ArrayList();
+    }
+
+    public static List getListForSet() {
+        return new ArrayList();
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList();
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
