582832358b8b9177d2ba96d99ce3a930836871df
Lovrus <lovrus120581@gmail.com>
2022-07-02 23:26:05 +0300

    JR_14_7_3

---
+++ src/DomZad/JR_14_7_3.java
@@ -0,0 +1,50 @@
+package DomZad;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Одинаковые имя и фамилия
+*/
+
+public class JR_14_7_3 {
+    public static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Сильвестр");
+        map.put("Иванов", "Дмитрий");
+        map.put("Кузнецов", "Арнольд");
+        map.put("Соколов", "Роман");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Илья");
+        map.put("Петров", "Алексей");
+        return map;
+    }
+
+    public static int getCountTheSameFirstName(Map<String, String> map, String name) {
+        int count = 0;
+        for (String nameTmp : map.values()) {
+            if (nameTmp.equals(name)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
+        int count = 0;
+        for (String lastNameTmp : map.keySet()) {
+            if (lastNameTmp.equals(lastName)) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
