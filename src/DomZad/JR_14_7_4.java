7f7c5430a5f519b6f36c04025323750c31a161aa
Lovrus <lovrus120581@gmail.com>
2022-07-03 23:54:02 +0300

    JR_14_7_4

---
+++ src/DomZad/JR_14_7_4.java
@@ -0,0 +1,54 @@
+package DomZad;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Удалить людей, имеющих одинаковые имена
+*/
+
+public class JR_14_7_4 {
+    public static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Сильвестр");
+        map.put("Иванов", "Дмитрий");
+        map.put("Кузнецов", "Арнольд");
+        map.put("Соколов", "Роман");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Илья");
+        map.put("Петров", "Алексей");
+        return map;
+    }
+
+    public static void removeTheFirstNameDuplicates(Map<String, String> map) {
+        Map<String, String> copy = new HashMap<>(map);
+
+        for (String name : copy.values()) {
+            int count = 0;
+            for (String nameTmp : map.values()) {
+                if (nameTmp.equals(name)) {
+                    count++;
+                }
+            }
+            if (count > 1) {
+                removeItemFromMapByValue(map, name);
+            }
+        }
+    }
+
+    public static void removeItemFromMapByValue(Map<String, String> map, String value) {
+        Map<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getValue().equals(value)) {
+                map.remove(pair.getKey());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
