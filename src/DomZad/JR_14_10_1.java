239dc25585cd5a6e8b31011beb2c7457d5cb85a4
Lovrus <lovrus120581@gmail.com>
2022-07-06 22:18:57 +0300

    JR_14_10_1

---
+++ src/DomZad/JR_14_10_1.java
@@ -0,0 +1,36 @@
+package DomZad;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Set из котов
+*/
+
+public class JR_14_10_1 {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        cats.remove(cats.iterator().next());
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+
+        return result;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        for (Cat cat : cats) {
+            System.out.println(cat);
+        }
+    }
+
+    public static class Cat {
+
+    }
+
+}
