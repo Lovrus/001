c1995e816f93ee751787da39d8a596482c3a5f92
Lovrus <lovrus120581@gmail.com>
2022-07-08 21:24:17 +0300

    JR_14_10_2

---
+++ src/DomZad/JR_14_10_3.java
@@ -0,0 +1,37 @@
+package DomZad;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Однофамильцы и тёзки
+*/
+
+public class JR_14_10_3 {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
+
+    public static Map<String, String> createPeopleMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Роман");
+        map.put("Иванов", "Дмитрий");
+        map.put("Кузнецов", "Арнольд");
+        map.put("Соколов", "Роман");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Илья");
+        map.put("Новиков", "Алексей");
+
+        return map;
+    }
+
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+}
