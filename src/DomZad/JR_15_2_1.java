cafa8bb7efbe2f44c2cd69a76b5e4ed44569d907
Lovrus <lovrus120581@gmail.com>
2022-07-12 21:44:33 +0300

    JR_15_2_1

---
+++ src/DomZad/JR_15_2_1.java
@@ -0,0 +1,34 @@
+package DomZad;
+/*
+Возвращаем StackTrace
+*/
+
+public class JR_15_2_1 {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static StackTraceElement[] method1() {
+        method2();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method2() {
+        method3();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method3() {
+        method4();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method4() {
+        method5();
+        return Thread.currentThread().getStackTrace();
+    }
+
+    public static StackTraceElement[] method5() {
+        return Thread.currentThread().getStackTrace();
+    }
+}
