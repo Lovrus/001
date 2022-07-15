49c43f77cfe0124df58556ce87061dba61920900
Lovrus <lovrus120581@gmail.com>
2022-07-15 23:07:59 +0300

    LinkedListDemo

---
+++ src/chapter_1_19/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package chapter_1_19;
+// Продемонстрировать применение класса LinkedList
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        // созать связный список
+        LinkedList<String> ll = new LinkedList<String>();
+
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+
+    }
+}
