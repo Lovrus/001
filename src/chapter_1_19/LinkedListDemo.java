a2a8536ce52db28d4aaeee5bbde103cc526e3a6d
Lovrus <lovrus120581@gmail.com>
2022-07-16 00:31:37 +0300

    LinkedListDemo-2

--- src/chapter_1_19/LinkedListDemo.java
+++ src/chapter_1_19/LinkedListDemo.java
@@ -13,6 +13,31 @@ public class LinkedListDemo {
         ll.add("D");
         ll.add("E");
         ll.add("C");
+        ll.addLast("Z");
+        ll.addFirst("A");
 
+        ll.add(1, "A2");
+
+        System.out.println("Исходное содержимое связного "
+                + "списка ll: " + ll);
+
+        ll.remove("F"); // удалить указанные элементы
+        ll.remove(2); // из связного списка
+        System.out.println("Содержимое связного списка ll "
+                + "после удаления элеметов: " + ll);
+
+        ll.removeFirst(); // удалить первый и последний
+        ll.removeLast(); // элементы из связного списка
+
+        System.out.println("Содержимое связного списка ll "
+                + "после удаления первого и "
+                + "последнего элементов: " + " " + ll);
+
+        // получить и присвоить значение
+        String val = ll.get(2);
+        ll.set(2, val + " изменено");
+
+        System.out.println("Содержимое связного списка ll "
+                + "после изменения: " + ll);
     }
 }
