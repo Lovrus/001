c12b9467ba7a40177d24205836eb16b0bcb826b3
Lovrus <lovrus120581@gmail.com>
2022-08-08 23:40:34 +0300

    PropDemo

---
+++ src/chapter_1_19/PropDemo.java
@@ -0,0 +1,11 @@
+package chapter_1_19;
+// Продемонстрировать примение списка свойств
+import java.util.*;
+
+public class PropDemo {
+    public static void main(String[] args) {
+        Properties capitals = new Properties();
+        capitals.put("Иллинойс", "Спрингфилд"):
+
+    }
+}
