ff0a7f0bf08e4b7f6adb433264c10276f8ea4860
Lovrus <lovrus120581@gmail.com>
2022-08-18 23:11:57 +0300

    JR_15_4_2

---
+++ src/DomZad/JR_15_4_2.java
@@ -0,0 +1,12 @@
+package DomZad;
+
+import java.io.FileNotFoundException;
+
+import static DomZad.JR_15_4_1.method1;
+
+public class JR_15_4_2 {
+    public static void method2() throws FileNotFoundException, ClassNotFoundException {
+        method1();
+    }
+
+}
