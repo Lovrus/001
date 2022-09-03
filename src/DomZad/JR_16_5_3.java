6ef43368c1611970b8bec12b47e80c249f82b76b
lovrus <lovrus120581@gmail.com>
2022-09-03 09:16:38 +0300

    JR_16_5_3

---
+++ src/DomZad/JR_16_5_3.java
@@ -0,0 +1,28 @@
+package DomZad;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/*
+Пробрасываем исключения
+*/
+
+public class JR_16_5_3 {
+
+    public void method1() throws IOException {
+
+    }
+
+    public void method2() throws FileNotFoundException {
+
+    }
+
+    public void method3() throws NullPointerException {
+
+    }
+
+    public void method4() throws NumberFormatException {
+
+    }
+}
+
