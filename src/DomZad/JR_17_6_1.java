0a454025e9cb8b15d9d31be94806a8558ea77c27
lovrus <lovrus120581@gmail.com>
2022-09-09 16:18:18 +0300

    JR_17_6_1

---
+++ src/DomZad/JR_17_6_1.java
@@ -0,0 +1,17 @@
+package DomZad;
+/*
+Лошадь и Пегас
+*/
+
+public class JR_17_6_1 {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
