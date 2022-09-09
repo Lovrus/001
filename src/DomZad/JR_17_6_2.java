a3b140031c163ce217e6b4391a0a328c567bee75
lovrus <lovrus120581@gmail.com>
2022-09-09 23:13:53 +0300

    JR_17_6_2

---
+++ src/DomZad/JR_17_6_2.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Домашние животные
+*/
+
+public class JR_17_6_2 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
