c508ceaa69ee22b7dda4519c61560cbc6501a755
lovrus <lovrus120581@gmail.com>
2022-09-10 22:04:12 +0300

    JR_17_6_4

---
+++ src/DomZad/JR_17_6_3.java
@@ -0,0 +1,34 @@
+package DomZad;
+/*
+Межпланетная ферма
+*/
+
+public class JR_17_6_3 {
+    public static void main(String[] args) {
+    }
+
+    public class Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Sheep extends Animal {
+
+    }
+
+    public class Bull extends Animal {
+
+    }
+
+    public class Goat extends Animal {
+
+    }
+
+}
---
+++ src/DomZad/JR_17_6_4.java
@@ -0,0 +1,25 @@
+package DomZad;
+/*
+Хороши работнички
+*/
+
+public class JR_17_6_4 {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
