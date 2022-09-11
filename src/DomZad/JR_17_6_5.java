9bad3dd8d529112160c328df4eba6f3c111e50ff
lovrus <lovrus120581@gmail.com>
2022-09-11 17:28:52 +0300

    JR_17_6_5

---
+++ src/DomZad/JR_17_6_5.java
@@ -0,0 +1,46 @@
+package DomZad;
+/*
+ИТ-компания
+*/
+
+public class JR_17_6_5 {
+    public static void main(String[] args) {
+    }
+
+    public class Worker {
+
+    }
+
+    public class Clerk extends Worker {
+
+    }
+
+    public class IT extends Worker {
+
+    }
+
+    public class Programmer extends IT {
+
+    }
+
+    public class ProjectManager extends IT {
+
+    }
+
+    public class CTO extends IT {
+
+    }
+
+    public class OfficeManager extends Clerk {
+
+    }
+
+    public class HR extends Clerk {
+
+    }
+
+    public class Cleaner extends Clerk {
+
+    }
+}
+
