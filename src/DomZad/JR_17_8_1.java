edd767587ecc5f588518b9b8173b726e31c671fa
lovrus <lovrus120581@gmail.com>
2022-09-11 17:31:21 +0300

    JR_17_8_2

---
+++ src/DomZad/JR_17_8_1.java
@@ -0,0 +1,24 @@
+package DomZad;
+/*
+Скрытный инкапсулированный кот
+*/
+
+public class JR_17_8_1 {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+        private String name;
+        private int age;
+        private int weight;
+
+        public Cat() {
+        }
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+    }
+}
