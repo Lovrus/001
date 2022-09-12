e8991e1d221a350507c9ef3c1772d6c8a4abd1bc
lovrus <lovrus120581@gmail.com>
2022-09-12 16:03:08 +0300

    JR_17_11_1

---
+++ src/DomZad/JR_17_11_1.java
@@ -0,0 +1,19 @@
+package DomZad;
+/*
+«Из ребра его»
+*/
+
+public class JR_17_11_1 {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
