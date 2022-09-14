e10c0209bbb90b60c53602a0ed7798d40ab40eb7
lovrus <lovrus120581@gmail.com>
2022-09-14 03:27:57 +0300

    JR_17_11_7

---
+++ src/DomZad/JR_17_11_6.java
@@ -0,0 +1,22 @@
+package DomZad;
+/*
+Цепочка наследования
+*/
+
+public class JR_17_11_6 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+}
---
+++ src/DomZad/JR_17_11_7.java
@@ -0,0 +1,29 @@
+package DomZad;
+/*
+Альтернативная цепочка наследования
+*/
+
+public class JR_17_11_7 {
+    public static void main(String[] args) {
+    }
+
+    public class Carnivore extends Animal {
+
+    }
+
+    public class Cow extends Animal {
+
+    }
+
+    public class Pig extends Animal {
+
+    }
+
+    public class Dog extends Carnivore {
+
+    }
+
+    public class Animal {
+
+    }
+}
