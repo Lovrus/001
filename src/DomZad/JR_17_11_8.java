67553f639201ed0662304e83c73bff972855e1f7
lovrus <lovrus120581@gmail.com>
2022-09-15 01:01:38 +0300

    JR_17_11_9

---
+++ src/DomZad/JR_17_11_8.java
@@ -0,0 +1,26 @@
+package DomZad;
+/*
+Городское хозяйство
+*/
+
+public class JR_17_11_8 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+}
---
+++ src/DomZad/JR_17_11_9.java
@@ -0,0 +1,26 @@
+package DomZad;
+/*
+Четвертая правильная «цепочка наследования»
+*/
+
+public class JR_17_11_9 {
+    public static void main(String[] args) {
+    }
+
+    public class House {
+
+    }
+
+    public class Cat {
+
+    }
+
+    public class Car {
+
+    }
+
+    public class Dog {
+
+    }
+}
+
