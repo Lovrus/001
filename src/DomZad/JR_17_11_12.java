61fc887504943a459e230887f4cbc1b0b9197fcb
lovrus <lovrus120581@gmail.com>
2022-09-17 13:07:31 +0300

    JR_17_11_13

---
+++ src/DomZad/JR_17_11_12.java
@@ -0,0 +1,30 @@
+package DomZad;
+/*
+Помощь шахматной школе
+*/
+
+public class JR_17_11_12 {
+    public static void main(String[] args) {
+    }
+
+    public class ChessPieces {
+    }
+
+    public class King extends ChessPieces {
+    }
+
+    public class Queen extends ChessPieces {
+    }
+
+    public class Rook extends ChessPieces {
+    }
+
+    public class Knight extends ChessPieces {
+    }
+
+    public class Bishop extends ChessPieces {
+    }
+
+    public class Pawn extends ChessPieces {
+    }
+}
