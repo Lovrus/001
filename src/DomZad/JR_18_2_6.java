5bfc5a0ccb013c0a3b96ebdd8423a8b25c107f4f
lovrus <lovrus120581@gmail.com>
2022-09-20 01:12:46 +0300

    JR_18_2_7

---
+++ src/DomZad/JR_18_2_6.java
@@ -0,0 +1,18 @@
+package DomZad;
+/*
+Выполняем перегрузку!
+*/
+
+public class JR_18_2_6 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+}
---
+++ src/DomZad/JR_18_2_7.java
@@ -0,0 +1,20 @@
+package DomZad;
+
+/*
+Int и Integer
+*/
+
+public class JR_18_2_7 {
+    public static void main(String[] args) {
+        Integer s = 1;
+        print(2);
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer s) {
+
+    }
+}
