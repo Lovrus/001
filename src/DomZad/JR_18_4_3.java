fb069e613b5b74349327dbbf3291aa8592d888ae
lovrus <lovrus120581@gmail.com>
2022-09-21 00:29:19 +0300

    JR_18_4_3

---
+++ src/DomZad/JR_18_4_3.java
@@ -0,0 +1,25 @@
+package DomZad;
+/*
+Свобода печати
+*/
+
+public class JR_18_4_3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+    }
+
+    public static void print(double a) {
+    }
+
+    public static void print(long a) {
+    }
+
+    public static void print(float a) {
+    }
+
+    public static void print(Integer a) {
+    }
+}
