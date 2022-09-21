ada777f797b2822fe48daa6aec26acf8e78de357
lovrus <lovrus120581@gmail.com>
2022-09-22 00:22:21 +0300

    JR_18_4_5

---
+++ src/DomZad/JR_18_4_4.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Три метода и минимум
+*/
+
+public class JR_18_4_4 {
+    public static void main(String[] args) {
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static long min(long a, long b) {
+        return Math.min(a, b);
+    }
+
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+}
---
+++ src/DomZad/JR_18_4_5.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Три метода и максимум
+*/
+
+class JR_18_4_5 {
+    public static void main(String[] args) {
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
+
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+}
