03391a2f1e47d52fd6aa162a162cc8903cdd08a5
lovrus <lovrus120581@gmail.com>
2022-09-27 00:15:21 +0300

    JR_18_9_1

---
+++ src/DomZad/JR_18_9_1.java
@@ -0,0 +1,17 @@
+package DomZad;
+/*
+Летать охота
+*/
+
+public class JR_18_9_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        boolean canFly();
+
+        void fly();
+    }
+}
+
