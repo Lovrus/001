b06ea1512bbfe9c8c89a5f23a1c0cbf97c6358b9
lovrus <lovrus120581@gmail.com>
2022-09-28 00:03:43 +0300

    JR_18_9_2

---
+++ src/DomZad/JR_18_9_2.java
@@ -0,0 +1,22 @@
+package DomZad;
+/*
+Лететь, бежать и плыть
+*/
+
+public class JR_18_9_2 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+    }
+
+    public interface CanRun {
+        void run();
+    }
+
+    public interface CanSwim {
+        void swim();
+    }
+}
