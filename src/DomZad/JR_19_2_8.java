e2c34c3357991333ee2bbd919a4e15405456d212
lovrus <lovrus120581@gmail.com>
2022-10-09 09:25:00 +0300

    JR_19_2_8

---
+++ src/DomZad/JR_19_2_8.java
@@ -0,0 +1,16 @@
+package DomZad;
+/*
+Эй, ты там живой?
+*/
+
+public class JR_19_2_8 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Person {
+        boolean isAlive();
+    }
+
+    public interface Presentable extends Person {
+    }
+}
