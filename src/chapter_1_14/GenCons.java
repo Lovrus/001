ec7b069ba50b5f99880c229941101420a7168061
lovrus <lovrus120581@gmail.com>
2022-10-13 00:27:21 +0300

    GenCons

---
+++ src/chapter_1_14/GenCons.java
@@ -0,0 +1,24 @@
+package chapter_1_14;
+
+// Использовать обобщенный конструктор
+public class GenCons {
+    private double val2;
+
+    <T extends Number> GenCons(T arg) {
+        val2 = arg.doubleValue();
+    }
+
+    void showval() {
+        System.out.println("val: "+ val2);
+    }
+}
+
+class GenConsDemo {
+    public static void main(String[] args) {
+
+        GenCons test = new GenCons(100);
+        GenCons test2 = new GenCons(123.5F);
+        test.showval();
+        test2.showval();
+    }
+}
