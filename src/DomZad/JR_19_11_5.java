ff6129ea62c8569f8bf6d3f9c3e3077fcc6f4c32
lovrus <lovrus120581@gmail.com>
2022-10-19 06:46:52 +0300

    JR_19_11_5

---
+++ src/DomZad/JR_19_11_5.java
@@ -0,0 +1,28 @@
+package DomZad;
+/*
+Исправление ошибок
+*/
+
+public class JR_19_11_5 {
+    public static void main(String[] args) {
+        System.out.println(Dream.HOBBY.toString());
+        System.out.println(new Hobby().INDEX);
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        Hobby HOBBY = new Hobby();
+    }
+
+    static class Hobby implements Desire, Dream {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+}
