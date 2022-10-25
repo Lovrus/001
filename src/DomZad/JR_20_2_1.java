2d27028f029d2b2339bcaae6769e981073c45519
lovrus <lovrus120581@gmail.com>
2022-10-26 00:14:42 +0300

    JR_20_2_2

---
+++ src/DomZad/JR_20_2_1.java
@@ -0,0 +1,31 @@
+package DomZad;
+/*
+Bingo!
+*/
+
+public class JR_20_2_1 {
+    public static void main(String[] args) {
+        Object animal = new Tiger();
+
+        boolean isCat = animal instanceof Cat;
+        boolean isTiger = animal instanceof Tiger;
+        boolean isPet = animal instanceof Pet;
+
+        printResults(isCat, isTiger, isPet);
+    }
+
+    private static void printResults(boolean cat, boolean tiger, boolean pet) {
+        if (cat && tiger && pet) System.out.println("Bingo!");
+    }
+
+    static class Pet {
+    }
+
+    static class Cat extends Pet {
+    }
+
+    static class Tiger extends Cat {
+    }
+
+
+}
---
+++ src/DomZad/JR_20_2_2.java
@@ -0,0 +1,26 @@
+package DomZad;
+/*
+Bingo-2!
+*/
+
+public class JR_20_2_2 {
+    public static void main(String[] args) {
+        Cat cat = new TomCat();
+
+        boolean isCat = cat instanceof Cat;
+        boolean isMovable = cat instanceof CanMove;
+        boolean isTom = cat instanceof TomCat;
+
+        if (isCat && isMovable && isTom) System.out.println("Bingo!");
+    }
+
+    interface CanMove {
+    }
+
+    static class Cat implements CanMove {
+    }
+
+    static class TomCat extends Cat {
+
+    }
+}
