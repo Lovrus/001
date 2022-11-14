d7e71baf09da5ad4df49102f4e3082777af1b603
lovrus <lovrus120581@gmail.com>
2022-11-14 23:28:41 +0300

    JR_20_6_13

---
+++ src/DomZad/JR_20_6_13.java
@@ -0,0 +1,9 @@
+package DomZad;
+/*
+Singleton
+*/
+
+public class JR_20_6_13 {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/DomZad/Singleton.java
@@ -0,0 +1,12 @@
+package DomZad;
+
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    private Singleton() {
+    }
+}
