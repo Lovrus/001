d7f246b1898b2d61e24f062b8c3d60a5b45bd2de
lovrus <lovrus120581@gmail.com>
2022-11-17 22:32:19 +0300

    JR_21_2_3

---
+++ src/DomZad/JR_21_2_3.java
@@ -0,0 +1,45 @@
+package DomZad;
+/*
+ООП - машинки
+*/
+
+public class JR_21_2_3 {
+    public static void main(String[] args) {
+        new JR_21_2_3.LuxuriousCar().printlnDesire();
+        new JR_21_2_3.CheapCar().printlnDesire();
+        new JR_21_2_3.Ferrari().printlnDesire();
+        new JR_21_2_3.Lanos().printlnDesire();
+    }
+
+    public static class LuxuriousCar {
+        void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.LUXURIOUS_CAR);
+        }
+    }
+
+    public static class CheapCar {
+        private void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.CHEAP_CAR);
+        }
+    }
+
+    public static class Ferrari extends LuxuriousCar {
+        protected void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.FERRARI_NAME);
+        }
+    }
+
+    public static class Lanos extends CheapCar {
+        public void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.LANOS_NAME);
+        }
+    }
+
+    public static class Constants {
+        public static String WANT_STRING = "Я хочу ездить на ";
+        public static String LUXURIOUS_CAR = "роскошной машине";
+        public static String CHEAP_CAR = "дешевой машине";
+        public static String FERRARI_NAME = "Феррари";
+        public static String LANOS_NAME = "Ланосе";
+    }
+}
