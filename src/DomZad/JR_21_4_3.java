2d17db18e39f8b8dcf42dc113832d7d5148865c1
lovrus <lovrus120581@gmail.com>
2022-11-22 22:25:34 +0300

    JR_21_4_3

---
+++ src/DomZad/JR_21_4_3.java
@@ -0,0 +1,15 @@
+package DomZad;
+/*
+ООП - Перегрузка - убираем лишнее
+*/
+
+public class JR_21_4_3 {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером!");
+    }
+}
+
