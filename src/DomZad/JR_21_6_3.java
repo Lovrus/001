91feb68b8498693621ff16df797b818c41f15e43
lovrus <lovrus120581@gmail.com>
2022-11-27 21:58:52 +0300

    JR_21_6_3

---
+++ src/DomZad/JR_21_6_3.java
@@ -0,0 +1,18 @@
+package DomZad;
+/*
+Максимально простой код-3
+*/
+
+public class JR_21_6_3 {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+    }
+
+    public class Machine implements Runnable {
+    }
+
+    public class Car extends Machine {
+    }
+}
