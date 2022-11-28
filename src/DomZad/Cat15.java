9dafd401d841a22389e2b77db61070ded8106ce6
lovrus <lovrus120581@gmail.com>
2022-11-28 23:51:36 +0300

    Cat15

---
+++ src/DomZad/Cat15.java
@@ -0,0 +1,23 @@
+package DomZad;
+
+import java.util.Properties;
+
+public class Cat15 {
+    public static int catCount = 0;
+    public static String namePrefix;
+
+    static {
+        Properties p = new Properties();
+        p.loadFromFile("cat.properties");
+        namePrefix = p.get("name-prefix");
+    }
+
+    public static int maxCatCount = 50;
+
+    static {
+        Properties p = new Properties();
+        p.loadFromFile("max.properties");
+        if (p.get("cat-max") != null)
+            maxCatCount = p.getInt("cat-max");
+    }
+}
