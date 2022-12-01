33f767d09d16fbe2041894ba5e6640f1458363f3
lovrus <lovrus120581@gmail.com>
2022-12-01 23:48:41 +0300

    JR_21_9_3

---
+++ src/DomZad/JR_21_9_3.java
@@ -0,0 +1,25 @@
+package DomZad;
+/*
+Значения по умолчанию
+*/
+
+public class JR_21_9_3 {
+    int intVar;
+    double doubleVar;
+    Double DoubleVar;
+    boolean booleanVar;
+    Object ObjectVar;
+    Exception ExceptionVar;
+    String StringVar;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.intVar);
+        System.out.println(solution.doubleVar);
+        System.out.println(solution.DoubleVar);
+        System.out.println(solution.booleanVar);
+        System.out.println(solution.ObjectVar);
+        System.out.println(solution.ExceptionVar);
+        System.out.println(solution.StringVar);
+    }
+}
