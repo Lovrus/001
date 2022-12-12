cc96250cdd38f248829b2e68b60bcfd1c3835a56
lovrus <lovrus120581@gmail.com>
2022-12-12 23:06:36 +0300

    JR_21_12_9

---
+++ src/DomZad/JR_21_12_9.java
@@ -0,0 +1,28 @@
+package DomZad;
+/*
+Перегрузка конструкторов
+*/
+
+public class JR_21_12_9 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public JR_21_12_9() {
+
+    }
+
+    private JR_21_12_9(int i) {
+
+    }
+
+    protected JR_21_12_9(double i) {
+
+    }
+
+    JR_21_12_9(short i) {
+
+    }
+}
---
+++ src/DomZad/SubSolution.java
@@ -0,0 +1,14 @@
+package DomZad;
+
+public class SubSolution extends JR_21_12_9 {
+    public SubSolution() {
+    }
+
+    protected SubSolution(double i) {
+        super(i);
+    }
+
+    SubSolution(short i) {
+        super(i);
+    }
+}
