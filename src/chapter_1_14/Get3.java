38e690eae4100fdbc7afb5021f8cf332520ebcff
lovrus <lovrus120581@gmail.com>
2022-12-18 19:36:37 +0300

    Gen3

---
+++ src/chapter_1_14/Get3.java
@@ -0,0 +1,22 @@
+package chapter_1_14;
+
+// Простая иерархия обобщенных классов
+public class Get3<T> {
+    T ob;
+
+    Get3(T o) {
+        ob = o;
+    }
+
+    // возвратить объект ob
+    T getOb() {
+        return ob;
+    }
+}
+
+// Подкласс, производный от класса Gen
+class Gen4<T> extends Get3<T> {
+    Gen4(T o) {
+        super(o);
+    }
+}
