44a14a597e06f8c5afb2fb81846154691b2ecf57
lovrus <lovrus120581@gmail.com>
2023-01-05 23:47:26 +0300

    JR_23_10_2

---
+++ src/DomZad/JR_23_10_2.java
@@ -0,0 +1,38 @@
+package DomZad;
+/*
+Отсчет на гонках
+*/
+
+public class JR_23_10_2 {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isInterrupted() && numSeconds >= 0) {
+                    if (numSeconds == 0) {
+                        System.out.println("Марш!");
+                    } else {
+                        System.out.print(numSeconds + " ");
+                        Thread.sleep(1000);
+                    }
+                    numSeconds--;
+                }
+            } catch (InterruptedException e) {
+                if (numSeconds != -1) {
+                    System.out.println("Прервано!");
+                }
+            }
+        }
+    }
+}
