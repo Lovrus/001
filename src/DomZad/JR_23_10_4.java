00b7bf740e05a455cf7d2ca306176239783b70fe
lovrus <lovrus120581@gmail.com>
2023-01-07 23:36:24 +0300

    JR_23_10_4

---
+++ src/DomZad/JR_23_10_4.java
@@ -0,0 +1,31 @@
+package DomZad;
+/*
+А без interrupt слабо?
+*/
+
+public class JR_23_10_4 {
+    public static volatile boolean finish = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        finish = true;
+    }
+
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (!finish) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
