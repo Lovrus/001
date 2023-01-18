eb9332ddded197eb6d7775bff48ce1f94d40d59a
lovrus <lovrus120581@gmail.com>
2023-01-18 22:08:05 +0300

    JR_23_13_10

---
+++ src/DomZad/JR_23_13_10.java
@@ -0,0 +1,54 @@
+package DomZad;
+/*
+Создание по образцу
+*/
+
+public class JR_23_13_10 {
+    public static int number = 5;
+
+    public static void main(String[] args) {
+        new Thread(new CountdownRunnable(), "Уменьшаем").start();
+        new Thread(new CountUpRunnable(), "Увеличиваем").start();
+    }
+
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp;
+
+        public void run() {
+            try {
+                while (true) {
+                    ++countIndexUp;
+                    System.out.println(toString());
+                    Thread.sleep(500);
+                    if (countIndexUp == JR_23_13_10.number) return;
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
+    }
+
+
+    public static class CountdownRunnable implements Runnable {
+        private int countIndexDown = JR_23_13_10.number;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexDown -= 1;
+                    if (countIndexDown == 0) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexDown;
+        }
+    }
+}
