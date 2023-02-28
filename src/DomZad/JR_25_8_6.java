9ed74449f5ce07486b0f8d6ae31fbac05707e338
lovrus <lovrus120581@gmail.com>
2023-02-28 23:23:08 +0300

    JR_25_8_6

---
+++ src/DomZad/JR_25_8_6.java
@@ -0,0 +1,45 @@
+package DomZad;
+
+import java.util.List;
+
+/*
+Таблица
+*/
+
+public class JR_25_8_6 {
+    public class TableInterfaceWrapper implements TableInterface {
+
+        TableInterface tableInterface;
+
+        public TableInterfaceWrapper(TableInterface tableInterface) {
+            this.tableInterface = tableInterface;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            tableInterface.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return tableInterface.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            tableInterface.setHeaderText(newHeaderText);
+        }
+    }
+
+    public interface TableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+    }
+}
