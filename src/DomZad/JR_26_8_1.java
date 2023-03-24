21f4741c9faa7d8c5e9efa1ed1ed722151f19fd3
lovrus <lovrus120581@gmail.com>
2023-03-24 23:17:41 +0300

    JR_26_8_1

---
+++ src/DomZad/JR_26_8_1.java
@@ -0,0 +1,34 @@
+package DomZad;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/*
+Ридер обертка
+*/
+
+public class JR_26_8_1 {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream newPrintStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(newPrintStream);
+
+        testString.printSomething();
+
+        System.setOut(defaultPrintStream);
+
+        String result = byteArrayOutputStream.toString().toUpperCase();
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
