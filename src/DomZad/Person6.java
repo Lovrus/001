4e8a7228cd59b94a6ee4bed9552ccd85ac8c40d3
lovrus <lovrus120581@gmail.com>
2023-04-04 23:50:46 +0300

    JR_26_11_6

---
+++ src/DomZad/Person6.java
@@ -0,0 +1,21 @@
+package DomZad;
+
+import java.util.Date;
+
+public class Person6 {
+    private String name;
+    private Date birthDate;
+
+    public Person6(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+}
