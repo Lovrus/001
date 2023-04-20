0f420af547738c50608922fa5a35d36e21b2b86e
lovrus <lovrus120581@gmail.com>
2023-04-20 23:33:24 +0300

    JR_27_4_5

---
+++ src/DomZad/JR_27_4_5.java
@@ -0,0 +1,32 @@
+package DomZad;
+
+import java.io.Serializable;
+
+/*
+Как сериализовать что-то свое?
+*/
+
+public class JR_27_4_5 {
+    public static class Object implements Serializable {
+        public String string1;
+        public String string2;
+    }
+
+    public static int stringCount;
+
+    public static class String implements Serializable {
+        private final int number;
+
+        public String() {
+            number = ++stringCount;
+        }
+
+        public void print() {
+            System.out.println("string #" + number);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
