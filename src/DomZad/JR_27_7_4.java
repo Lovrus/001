6b8db5ec133d64bba66cb6d6468287908bf529ee
lovrus <lovrus120581@gmail.com>
2023-04-24 23:20:23 +0300

    JR_27_7_4

---
+++ src/DomZad/JR_27_7_4.java
@@ -0,0 +1,33 @@
+package DomZad;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+Serializable Solution
+*/
+
+public class JR_27_7_4 implements Serializable {
+    public static void main(String[] args) {
+        System.out.println(new Solution(4));
+    }
+
+    private transient final String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
+    String string;
+
+    public Solution(int temperature) {
+        this.currentDate = new Date();
+        this.temperature = temperature;
+
+        string = "Today is %s, and the current temperature is %s C";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        this.string = String.format(string, format.format(currentDate), temperature);
+    }
+
+    @Override
+    public String toString() {
+        return this.string;
+    }
+}
