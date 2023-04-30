65378e34c4406bedfa5cfb2a348486f43cde74b6
lovrus <lovrus120581@gmail.com>
2023-04-30 23:20:26 +0300

    JR_27_10_5

---
+++ src/DomZad/JR_27_10_5.java
@@ -0,0 +1,41 @@
+package DomZad;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/*
+Сериализация человека
+*/
+
+public class JR_27_10_5 {
+
+    public static class Person implements Serializable {
+        String firstName;
+        String lastName;
+        transient String fullName;
+        final transient String greeting = "Hello, ";
+        String country;
+        Sex sex;
+        transient PrintStream outputStream = System.out;
+        transient Logger logger;
+
+        Person(String firstName, String lastName, String country, Sex sex) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.fullName = String.format("%s, %s", lastName, firstName);
+            this.country = country;
+            this.sex = sex;
+            this.logger = Logger.getLogger(String.valueOf(Person.class));
+        }
+    }
+
+    enum Sex {
+        MALE,
+        FEMALE
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
