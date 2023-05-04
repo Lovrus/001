6542f10c741f42c82f69833b2cf38bb89205af44
lovrus <lovrus120581@gmail.com>
2023-05-04 23:52:00 +0300

    JR_27_10_9

---
+++ src/DomZad/JR_27_10_9.java
@@ -0,0 +1,18 @@
+package DomZad;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+Знакомство с графами
+*/
+
+public class JR_27_10_9 implements Serializable {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
