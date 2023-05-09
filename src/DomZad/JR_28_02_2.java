747be087231a89b4b95ddace999216b25c1c1b41
lovrus <lovrus120581@gmail.com>
2023-05-09 23:50:40 +0300

    JR_28_02_2

---
+++ src/DomZad/JR_28_02_2.java
@@ -0,0 +1,32 @@
+package DomZad;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+/*
+Сравниваем модификаторы
+*/
+
+public class JR_28_02_2 {
+    public static void main(String[] args) {
+        int classModifiers = Solution.class.getModifiers();
+        System.out.println(isModifierSet(classModifiers, Modifier.PUBLIC));   //true
+        System.out.println(isModifierSet(classModifiers, Modifier.STATIC));   //false
+
+        int methodModifiers = getMainMethod().getModifiers();
+        System.out.println(isModifierSet(methodModifiers, Modifier.STATIC));      //true
+    }
+
+    public static boolean isModifierSet(int allModifiers, int specificModifier) {
+        return (allModifiers & specificModifier) != 0;
+    }
+
+    private static Method getMainMethod() {
+        Method[] methods = Solution.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.getName().equalsIgnoreCase("main")) return method;
+        }
+
+        return null;
+    }
+}
