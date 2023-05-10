0e6f92b61c0196ad92649dc6fd9327879f0523c8
lovrus <lovrus120581@gmail.com>
2023-05-10 23:24:59 +0300

    JR_28_02_3

---
+++ src/DomZad/JR_28_02_3.java
@@ -0,0 +1,14 @@
+package DomZad;
+/*
+Все гениальное - просто!
+*/
+
+public class JR_28_02_3 {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
