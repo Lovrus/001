a4d71f0439ed30fdb8eb65c4dc02060ae450dfb2
lovrus <lovrus120581@gmail.com>
2023-05-20 23:46:48 +0300

    JR_28_10_03

---
+++ src/DomZad/JR_28_10_03.java
@@ -0,0 +1,13 @@
+package DomZad;
+
+public class JR_28_10_03 {
+    public static void main(String[] args) {
+        DBConnectionManager dbConnectionManager = new DBConnectionManager();
+        try (FakeConnection fakeConnection = dbConnectionManager.getFakeConnection()) {
+            System.out.println("Entering body of try block.");
+            fakeConnection.usefulOperation();
+            fakeConnection.unsupportedOperation();
+        } catch (Exception e) {
+        }
+    }
+}
