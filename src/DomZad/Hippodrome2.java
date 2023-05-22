d7ecbe24a93f0717639ad868485c0777c76f2382
lovrus <lovrus120581@gmail.com>
2023-05-22 23:45:29 +0300

    JR_28_16_02

---
+++ src/DomZad/Hippodrome2.java
@@ -0,0 +1,15 @@
+package DomZad;
+
+import java.util.List;
+
+public class Hippodrome2 {
+    private List<Horse2> horses;
+
+    public List<Horse2> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
