c897010002c9fe8f7a1e2eaf035ddfc1c90ae2a8
lovrus <lovrus120581@gmail.com>
2023-05-24 23:55:33 +0300

    JR_28_16_04

---
+++ src/DomZad/Hippodrome4.java
@@ -0,0 +1,18 @@
+package DomZad;
+import java.util.List;
+
+public class Hippodrome4 {
+    private List<Horse4> horses;
+
+    public Hippodrome4(List<Horse4> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse4> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/DomZad/Horse4.java
@@ -0,0 +1,6 @@
+package DomZad;
+public class Horse4 {
+    private String name;
+    private double speed;
+    private double distance;
+}
