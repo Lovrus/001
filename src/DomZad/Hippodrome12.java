93c2bef9becfb08e2d820e32be29df3e078073f6
lovrus <lovrus120581@gmail.com>
2023-06-01 23:58:13 +0300

    JR_28_16_12

---
+++ src/DomZad/Hippodrome12.java
@@ -0,0 +1,50 @@
+package DomZad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome12 {
+    static Hippodrome12 game;
+    private List<Horse> horses;
+
+    void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+    void move() {
+        for (Horse12 horse : horses) {
+            horse.move();
+        }
+    }
+
+    void print() {
+        for (Horse12 horse : horses) {
+            horse.print();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public Hippodrome12(List<Horse12> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse12> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        game = new Hippodrome12(new ArrayList<>());
+        game.getHorses().add(new Horse12("Lucky", 3, 0));
+        game.getHorses().add(new Horse12("Slevin", 3, 0));
+        game.getHorses().add(new Horse12("Homer", 3, 0));
+
+        game.run();
+    }
+}
---
+++ src/DomZad/Horse12.java
@@ -0,0 +1,45 @@
+package DomZad;
+
+public class Horse12 {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Horse12(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    void move() {
+        distance += speed * Math.random();
+    }
+
+    void print() {
+
+    }
+}
