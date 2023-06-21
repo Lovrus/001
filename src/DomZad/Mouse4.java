b70e043d8f771542dd659a2fbaa8adb62585a367
lovrus <lovrus120581@gmail.com>
2023-06-21 23:54:02 +0300

    JR_29_18_04

---
+++ src/DomZad/Mouse4.java
@@ -0,0 +1,19 @@
+package DomZad;
+
+public class Mouse4 {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Mouse4(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/DomZad/Room4.java
@@ -0,0 +1,7 @@
+package DomZad;
+
+public class Room4 {
+    public static void main(String[] args) {
+    }
+}
+
