d4f287403d7da3d55b67e41a9765a3f92058ffb8
lovrus <lovrus120581@gmail.com>
2023-07-09 23:10:17 +0300

    JR_30_2_1

---
+++ src/DomZad/JR_30_2_1.java
@@ -0,0 +1,21 @@
+package DomZad;
+/*
+Запрети наследование
+*/
+
+public class JR_30_2_1 {
+
+    public static final class Listener {
+        public void onMouseDown(int x, int y) {
+            // Do something when the mouse down event occurs
+        }
+
+        public void onMouseUp(int x, int y) {
+            // Do something when the mouse up event occurs
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
