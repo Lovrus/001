c6eb9af6ed536e34e79534632521b952c53a539e
lovrus <lovrus120581@gmail.com>
2023-07-28 23:55:39 +0300

    JR_30_13_10

---
+++ src/DomZad/Brick10.java
@@ -0,0 +1,16 @@
+package DomZad;
+public class Brick10 extends BaseObject10 {
+    public Brick10(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    void draw(Canvas10 canvas) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
+}
