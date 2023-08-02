a8b9f25c849019d305ff4492debfbff0153dd5e2
lovrus <lovrus120581@gmail.com>
2023-08-02 23:44:30 +0300

    JR_30_13_14

---
+++ src/DomZad/Brick14.java
@@ -0,0 +1,15 @@
+package DomZad;
+public class Brick14 extends BaseObject14 {
+    public Brick14(double x, double y) {
+        super(x, y, 3);
+    }
+
+    void draw(Canvas14 canvas) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
+}
