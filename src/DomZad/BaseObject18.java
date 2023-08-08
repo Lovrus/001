2ea367443292b678551778056117d979a8cdc7f4
lovrus <lovrus120581@gmail.com>
2023-08-09 00:03:47 +0300

    JR_30_13_18

---
+++ src/DomZad/BaseObject18.java
@@ -0,0 +1,48 @@
+package DomZad;
+public abstract class BaseObject18 {
+    protected double x;
+    protected double y;
+    protected double radius;
+
+    public BaseObject18(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    abstract void draw(Canvas18 canvas);
+
+    abstract void move();
+
+    boolean intersects(BaseObject18 o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
+}
