1259c297398b2411825f034909fabfe2c3bf21bf
lovrus <lovrus120581@gmail.com>
2023-08-20 23:24:55 +0300

    JR_31_2_3

---
+++ src/DomZad/JR_31_2_3.java
@@ -0,0 +1,31 @@
+package DomZad;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.util.ArrayDeque;
+import java.util.EventListener;
+
+/*
+Так-с... сопоставим
+*/
+
+public class JR_31_2_3 {
+    public static class A implements Serializable {
+    }
+
+    public static class B implements Remote {
+    }
+
+    public static class C extends ArrayDeque {
+        public C clone() {
+            return (C) super.clone();
+        }
+    }
+
+    public static class D implements EventListener {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
