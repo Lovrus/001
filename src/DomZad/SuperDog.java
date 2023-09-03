03533004608aed064584923c40ebd164b43cd88b
lovrus <lovrus120581@gmail.com>
2023-09-03 22:36:40 +0300

    JR_31_6_2

---
+++ src/DomZad/SuperDog.java
@@ -0,0 +1,9 @@
+package DomZad;
+public abstract class SuperDog {
+    protected String getSuperQuotes() {
+        //some logic here
+        return " *** ";
+    }
+
+    protected SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy EEE");
+}
