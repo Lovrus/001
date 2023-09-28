ef61a94896616e2efddcf06b2fe5b61df34242a0
lovrus <lovrus120581@gmail.com>
2023-09-28 23:24:27 +0300

    JR_31_14_2

---
+++ src/DomZad/ModelData2.java
@@ -0,0 +1,13 @@
+package DomZad;
+
+public class ModelData2 {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
