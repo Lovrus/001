e800a14a509c63cda6557ed3efe864ce6b4fd71b
lovrus <lovrus120581@gmail.com>
2023-10-17 23:40:56 +0300

    JR_31_14_4

---
+++ src/DomZad/JR_31_14_4.java
@@ -0,0 +1,15 @@
+package DomZad;
+
+public class JR_31_14_4 {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+
+        usersView.fireEventShowAllUsers();
+    }
+}
