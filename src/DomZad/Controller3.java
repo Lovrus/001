3f73866b4937bb6fbf5eef51d64442b9e4099627
lovrus <lovrus120581@gmail.com>
2023-10-18 23:32:11 +0300

    JR_31_14_4

---
+++ src/DomZad/Controller3.java
@@ -0,0 +1,22 @@
+package DomZad;
+
+public class Controller3 {
+    //controller cannot contain business logic, only operate with views
+
+    private Model model;
+    private UsersView usersView;
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+    public void onShowAllUsers() {
+        model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+}
+}
