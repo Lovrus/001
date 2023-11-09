93e25b487ecd9e97aa881821c89c2bd5c6c8c079
lovrus <lovrus120581@gmail.com>
2023-11-09 23:40:32 +0300

    JR_31_14_5

---
+++ src/DomZad/ModelData6.java
@@ -0,0 +1,16 @@
+package DomZad;
+
+import java.util.List;
+
+public class ModelData6 {
+
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
