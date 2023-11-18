364bc98e1e31fdb840e839bb0edbd8b0a7a6ec65
lovrus <lovrus120581@gmail.com>
2023-11-18 21:52:00 +0300

    JR_31_14_6

---
+++ src/DomZad/Controller5.java
@@ -0,0 +1,30 @@
+package DomZad;
+public class Controller5 {
+    //controller cannot contain business logic, only operate with views
+
+    private Model model;
+    private UsersView usersView;
+    private EditUserView editUserView;
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
+
+    public void onShowAllUsers() {
+        model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
+}
