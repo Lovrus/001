72553cfcd565a9fbf1d4a2a51aa202d239b74db2
lovrus <lovrus120581@gmail.com>
2023-11-19 22:51:29 +0300

    JR_31_14_6

---
+++ src/DomZad/UserService3.java
@@ -0,0 +1,17 @@
+package DomZad;
+
+public interface UserService3 {
+    User deleteUser(long id);
+
+    User createOrUpdateUser(String name, long id, int level);
+
+    List<User> getUsersByName(String name);
+
+    List<User> getAllDeletedUsers();
+
+    List<User> getUsersBetweenLevels(int fromLevel, int toLevel);
+
+    List<User> filterOnlyActiveUsers(List<User> allUsers);
+
+    User getUsersById(long userId);
+}
