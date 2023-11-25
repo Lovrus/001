5c53f8d91542381ccb1339f25b71b2b90d1b7000
lovrus <lovrus120581@gmail.com>
2023-11-25 23:44:41 +0300

    JR_31_14_6

---
+++ src/DomZad/UsersView4.java
@@ -0,0 +1,27 @@
+package DomZad;
+public class UsersView4 implements View {
+
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All " + (modelData.isDisplayDeletedUserList() ? "deleted " : "") + "users:");
+        for (User user : modelData.getUsers()) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+}
