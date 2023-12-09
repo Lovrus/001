73374b43ef2718ce7283261ce3937a140b128a9e
lovrus <lovrus120581@gmail.com>
2023-12-09 23:55:50 +0300

    JR_31_14_7

---
+++ src/DomZad/MainModel4.java
@@ -0,0 +1,38 @@
+package DomZad;
+import java.util.List;
+
+public class MainModel4 implements Model {
+
+    //use helpful services
+    private UserService userService = new UserServiceImpl();
+
+    //use special object to keep data for view rendering
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = userService.getUsersBetweenLevels(1, 100);
+        //refresh model data
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        //refresh model data
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+}
