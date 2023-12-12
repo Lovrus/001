6e2a2121768971c7854668e9adf62959fef6d121
lovrus <lovrus120581@gmail.com>
2023-12-12 22:37:35 +0300

    JR_31_14_7

---
+++ src/DomZad/EditUserView2.java
@@ -0,0 +1,17 @@
+package DomZad;
+
+public class EditUserView2 {
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
