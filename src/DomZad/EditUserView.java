c041e2f91613e53b5cb41bd95ce5ddc2c63ec3f8
lovrus <lovrus120581@gmail.com>
2023-12-20 00:04:33 +0300

    Исправления5

--- src/DomZad/EditUserView.java
+++ src/DomZad/EditUserView.java
@@ -4,7 +4,7 @@ public class EditUserView implements View5 {
     private Controller5 controller;
 
     @Override
-    public void refresh(ModelData modelData) {
+    public void refresh(ModelData7 modelData) {
         System.out.println("User to be edited:");
         System.out.println("\t" + modelData.getActiveUser());
         System.out.println("===================================================");
