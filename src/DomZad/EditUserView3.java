44eaed1f5f2bd3e67565c73193cd94a5977fbb4d
lovrus <lovrus120581@gmail.com>
2024-01-04 23:27:14 +0300

    JR_31_14_8_12

---
+++ src/DomZad/EditUserView3.java
@@ -0,0 +1,22 @@
+package DomZad;
+
+public class EditUserView3 {
+
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventUserDeleted(long id) {
+        controller.onUserDelete(id);
+    }
+}
