bfa0ae72ca8e29debee357bc738af43ac88d6fb8
lovrus <lovrus120581@gmail.com>
2024-01-05 23:57:00 +0300

    JR_31_14_8_13

--- src/chapter_1_5/ForTick2.java
+++ src/chapter_1_5/ForTick2.java
@@ -5,7 +5,7 @@ public class ForTick2 {
     public static void main(String[] args) {
 
         // здесь переменная n объявляется в самом цикле for
-        for (int n = 0; n > 0; n--)
+        for (int n = 10; n > 0; n--)
             System.out.println("такт" + n);
     }
 }
