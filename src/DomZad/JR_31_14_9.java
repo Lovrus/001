617008a2e7e80b653effd53686f7ed9cf6d4dbfe
lovrus <lovrus120581@gmail.com>
2024-01-12 22:44:51 +0300

    JR_31_14_9

---
+++ src/DomZad/JR_31_14_9.java
@@ -0,0 +1,24 @@
+package DomZad;
+
+public class JR_31_14_9 {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+        UsersView usersView = new UsersView();
+        EditUserView editUserView = new EditUserView();
+        Controller controller = new Controller();
+
+        usersView.setController(controller);
+        editUserView.setController(controller);
+
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
+
+        //emulate user events
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126L);
+        editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged("NewName", 126L, 73);
+        usersView.fireEventShowDeletedUsers();
+    }
+}
