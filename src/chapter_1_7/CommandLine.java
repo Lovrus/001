e41d96910d2ff267ab36d18ac87f71d567e3925b
lovrus <lovrus120581@gmail.com>
2024-01-13 00:39:21 +0300

    CommandLine_new

--- src/chapter_1_7/CommandLine.java
+++ src/chapter_1_7/CommandLine.java
@@ -1,8 +1,9 @@
 package chapter_1_7;
+
 // Вывести все аргументы командной строки
 public class CommandLine {
     public static void main(String[] args) {
-        for (int i=0; i< args.length; i++)
+        for (int i = 0; i < args.length; i++)
             System.out.println("args[" + i + "]: " + args[i]);
     }
 }
