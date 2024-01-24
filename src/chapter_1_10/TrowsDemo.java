7ee39dc293749765ef43f0181c5048e3ee510766
lovrus <lovrus120581@gmail.com>
2024-01-25 00:01:10 +0300

    TrowsDemo_new

--- src/chapter_1_10/TrowsDemo.java
+++ src/chapter_1_10/TrowsDemo.java
@@ -1,15 +1,14 @@
 package chapter_1_10;
 
-/* // Эта программа содержит ошибку,
+// Эта программа содержит ошибку,
 // и поэтому она не подлежит компиляции
 public class TrowsDemo {
     static void throwOne() {
         System.out.println("В теле метода throwOne()");
-        throw new IllegalAccessException("Демонстрация");
+        //throw new IllegalAccessException("Демонстрация");
     }
 
     public static void main(String[] args) {
         throwOne();
     }
 }
-*/
