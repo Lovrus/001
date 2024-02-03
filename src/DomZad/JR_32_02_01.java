3bb9344fcd1cc870c02a86b677b03fa8a66aaf5f
lovrus <lovrus120581@gmail.com>
2024-02-03 23:38:23 +0300

    JR_32_02_01

---
+++ src/DomZad/JR_32_02_01.java
@@ -0,0 +1,33 @@
+package DomZad;
+
+/*
+Свой enum
+*/
+
+public class JR_32_02_01 {
+    /**
+     * Output:
+     * <p/>
+     * Available Amount
+     * Account Number
+     * Bank Name
+     * --------------------
+     * Available Amount
+     * Bank Name
+     */
+    public static void main(String[] args) {
+
+        Column.configureColumns(Column.Amount, Column.AccountNumber, Column.BankName);
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+
+        System.out.println("--------------------");
+        Column.AccountNumber.hide();
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+    }
+}
