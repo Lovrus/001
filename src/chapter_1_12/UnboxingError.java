8b078c5e2dfd2190db038e443ee6777a65555067
lovrus <lovrus120581@gmail.com>
2024-02-06 22:36:16 +0300

    UnboxingError

---
+++ src/chapter_1_12/UnboxingError.java
@@ -0,0 +1,14 @@
+package chapter_1_12;
+
+// Ошибка, порождаемая ручной распаковкой
+public class UnboxingError {
+    public static void main(String[] args) {
+
+        Integer iOb = 1000; // автоупаковка значения 1000
+
+        int i = iOb.byteValue(); // ручная распаковка значения
+        // как относящегося к типу byte !!!
+
+        System.out.println(i); // значение 1000 не выводится!
+    }
+}
