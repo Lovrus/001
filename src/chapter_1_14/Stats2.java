7fd48e19ec4a09172036bfa372a3458c50f4ce04
lovrus <lovrus120581@gmail.com>
2024-02-16 22:13:10 +0300

    Stats2_new

--- src/chapter_1_14/Stats2.java
+++ src/chapter_1_14/Stats2.java
@@ -25,12 +25,12 @@ public class Stats2<T extends Number> {
 // продемонстрировать применение класса Stats2
 class BoundsDemo {
     public static void main(String[] args) {
-        Integer inums[] = {1, 2, 3, 4, 5};
+        Integer[] inums = {1, 2, 3, 4, 5};
         Stats2<Integer> iob = new Stats2<Integer>(inums);
         double v = iob.average();
         System.out.println("Среднее значение iob равно " + v);
 
-        Double dnums[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+        Double[] dnums = {1.1, 2.2, 3.3, 4.4, 5.5};
         Stats2<Double> dob = new Stats2<Double>(dnums);
         double w = dob.average();
         System.out.println("Среднее значение dob равно " + w);
