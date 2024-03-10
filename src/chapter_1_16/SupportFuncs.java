7d042bb4aa3f1c0362cedf5c2534541a02d46a24
lovrus <lovrus120581@gmail.com>
2024-03-10 23:16:49 +0300

    SupportFuncs

---
+++ src/chapter_1_16/SupportFuncs.java
@@ -0,0 +1,10 @@
+package chapter_1_16;
+
+// Вспомогательные функции.
+public class SupportFuncs {
+    // Выяснить, является ли а делителем b.
+    public static boolean isFactor(int a, int b) {
+        if ((b % a) == 0) return true;
+        return false;
+    }
+}
