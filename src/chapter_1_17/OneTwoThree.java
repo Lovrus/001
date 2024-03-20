a833d196464837b402fce53bf44225b0066ca2ff
lovrus <lovrus120581@gmail.com>
2024-03-20 20:32:03 +0300

    OneTwoThree

---
+++ src/chapter_1_17/OneTwoThree.java
@@ -0,0 +1,13 @@
+package chapter_1_17;
+
+public class OneTwoThree {
+    public static void main(String[] args) {
+        String str = """
+                один \
+                два
+                три \
+                четыре
+                """;
+        System.out.println(str);
+    }
+}
