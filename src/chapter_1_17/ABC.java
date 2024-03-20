057171f4b62fa2e537498501793cae7bb110e1c0
lovrus <lovrus120581@gmail.com>
2024-03-20 20:20:26 +0300

    ABC

---
+++ src/chapter_1_17/ABC.java
@@ -0,0 +1,24 @@
+package chapter_1_17;
+
+public class ABC {
+    public static void main(String[] args) {
+        String str = """
+                A
+                  B
+                C
+            """;  // определяет начало отступа
+        String str2 = """
+                A
+                  B
+                C
+                """; // никакого влияния не оказывает
+        String str3 = """
+                A
+                  B
+                C
+               """; // приводит к удалению пробельных символов плоть до """
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
