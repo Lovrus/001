a97a95359b1a9a8ce2c2ce5bbcbc2868190263cd
lovrus <lovrus120581@gmail.com>
2024-03-24 23:44:29 +0300

    Age2

---
+++ src/chapter_2_18/Age2.java
@@ -0,0 +1,9 @@
+package chapter_2_18;
+
+public class Age2 {
+    public static void main(String[] args) {
+        int age = 9;
+        String s = "Ему " + age + " лет.";
+        System.out.println(s);
+    }
+}
