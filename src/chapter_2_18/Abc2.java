cdfae65bc8c2725b4dbd5c9748ed97e1138d48ef
lovrus <lovrus120581@gmail.com>
2024-03-24 23:30:33 +0300

    Abc2_new

--- src/chapter_2_18/Abc2.java
+++ src/chapter_2_18/Abc2.java
@@ -2,9 +2,7 @@ package chapter_2_18;
 
 public class Abc2 {
     public static void main(String[] args) {
-        char[] chars = {'a', 'b', 'c'};
-        String s1 = new String(chars);
-        String s2 = "abc"; // использовать строковый литерал.
+        // Исправлен.
         System.out.println("abc".length());
     }
 }
