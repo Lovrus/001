364e90b760888ae43c26a255aab3051e256cdc63
lovrus <lovrus120581@gmail.com>
2024-03-24 23:25:21 +0300

    Abc

---
+++ src/chapter_2_18/Abc.java
@@ -0,0 +1,9 @@
+package chapter_2_18;
+
+public class Abc {
+    public static void main(String[] args) {
+        char[] chars = {'a', 'b', 'c'};
+        String s = new String(chars);
+        System.out.println(s.length());
+    }
+}
