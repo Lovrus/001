efa75cc4871d6a2b2f9aec5c6d5b1fdc5518cd09
lovrus <lovrus120581@gmail.com>
2024-03-25 22:08:09 +0300

    Box

---
+++ src/chapter_2_18/Box.java
@@ -0,0 +1,28 @@
+package chapter_2_18;
+
+// Переопределение метода toString() для класса Box.
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    public String toString() {
+        return "Размеры коробки: " + width + " на " +
+                depth + " на " + height + ".";
+    }
+}
+
+class ToStringDemo {
+    public static void main(String[] args) {
+        Box b = new Box(10, 12, 14);
+        String s = "Коробка b: " + b; // конкатенация с объектом Box
+        System.out.println(b); // преобразование Box в строку
+        System.out.println(s);
+    }
+}
