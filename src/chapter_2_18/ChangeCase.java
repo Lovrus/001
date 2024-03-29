ec6428ad181a8b3904ef0224e833cfccd966caad
lovrus <lovrus120581@gmail.com>
2024-03-29 22:03:43 +0300

    ChangeCase

---
+++ src/chapter_2_18/ChangeCase.java
@@ -0,0 +1,13 @@
+package chapter_2_18;
+
+// Демонстрация использования toUpperCase() и toLowerCase().
+public class ChangeCase {
+    public static void main(String[] args) {
+        String s = "This is a test.";
+        System.out.println("Первоначальная строка: " + s);
+        String upper = s.toUpperCase();
+        String lower = s.toLowerCase();
+        System.out.println("Строка в верхнем регистре: " + upper);
+        System.out.println("Строка в нижнем регистре: " + lower);
+    }
+}
