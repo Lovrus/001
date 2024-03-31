d6fdc0acf1bc68a5c01c8c32682765fa818d61fd
lovrus <lovrus120581@gmail.com>
2024-03-31 10:12:41 +0300

    ReserveDemo

---
+++ src/chapter_2_18/ReserveDemo.java
@@ -0,0 +1,11 @@
+package chapter_2_18;
+
+// Использование reserve() для изменения на противоположный
+public class ReserveDemo {
+    public static void main(String[] args) {
+        StringBuffer s = new StringBuffer("abcdef");
+        System.out.println(s);
+        s.reverse();
+        System.out.println(s);
+    }
+}
