77b9bdab3298db19b4abc036710954574a08ee60
lovrus <lovrus120581@gmail.com>
2024-04-01 21:33:36 +0300

    IndexOfDemo

---
+++ src/chapter_2_18/IndexOfDemo.java
@@ -0,0 +1,12 @@
+package chapter_2_18;
+
+public class IndexOfDemo {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("one two one");
+        int i;
+        i = sb.indexOf("one");
+        System.out.println("Индекс первого вхождения: " + i);
+        i = sb.lastIndexOf("one");
+        System.out.println("Индекс последнего вхождения: " + i);
+    }
+}
