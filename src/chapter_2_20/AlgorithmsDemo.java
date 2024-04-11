6da5ce4c86b8d9ee40141cf4ac4679653729dffc
lovrus <lovrus120581@gmail.com>
2024-04-11 23:31:21 +0300

    AlgorithmsDemo

---
+++ src/chapter_2_20/AlgorithmsDemo.java
@@ -0,0 +1,32 @@
+package chapter_2_20;
+// Демонстрация работы разнообразных алгоритмов.
+
+import java.util.*;
+
+public class AlgorithmsDemo {
+    public static void main(String[] args) {
+        // Создать и иницилизировать связных список.
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+        ll.add(-8);
+        ll.add(20);
+        ll.add(-20);
+        ll.add(8);
+        // Создать компаратор с обратным порядком.
+        Comparator<Integer> r = Collections.reverseOrder();
+        // Сортировать список с использованием созданного компаратора.
+        Collections.sort(ll, r);
+        System.out.print("Список отсортирован в обратном порядке: ");
+        for (int i : ll)
+            System.out.print(i + " ");
+        System.out.println();
+        // Тасовать список.
+        Collections.shuffle(ll);
+        // Отобразить рандомизированный список.
+        System.out.print("Список перетасован: ");
+        for (int i : ll)
+            System.out.print(i + " ");
+        System.out.println();
+        System.out.println("Наименьшее значение: " + Collections.min(ll));
+        System.out.println("Наибольшее значение: " + Collections.max(ll));
+    }
+}
