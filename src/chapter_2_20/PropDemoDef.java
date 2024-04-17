89dd7634d72a12ba69f44e893057b616145c96db
lovrus <lovrus120581@gmail.com>
2024-04-17 21:17:10 +0300

    PropDemoDef

---
+++ src/chapter_2_20/PropDemoDef.java
@@ -0,0 +1,30 @@
+package chapter_2_20;
+// Использование стандартного списка свойств.
+
+import java.util.*;
+
+public class PropDemoDef {
+    public static void main(String[] args) {
+        Properties defList = new Properties();
+        defList.setProperty("Флорида", "Таллахаси");
+        defList.setProperty("Висконсин", "Мадисон");
+        Properties capitals = new Properties(defList);
+        capitals.setProperty("Иллинойс", "Спрингфилд");
+        capitals.setProperty("МИссури", "Джефферсон-Сити");
+        capitals.setProperty("Вашингтон", "Олимпия");
+        capitals.setProperty("Калифорния", "Сакраменто");
+        capitals.setProperty("Индиана", "Индианаполис");
+        // Получить представление в виде набора для ключей.
+        Set<?> states = capitals.keySet();
+        // Отобразить все штаты и их столицы.
+        for (Object name : states)
+            System.out.println("Столица штата " +
+                    name + " - " +
+                    capitals.getProperty((String) name)
+                    + ".");
+        System.out.println();
+        // Штат Флорида теперь будет найден в стандартном списке.
+        String str = capitals.getProperty("Флорида");
+        System.out.println("Столица штата Флорида - " + str + ".");
+    }
+}
