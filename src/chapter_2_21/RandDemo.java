8da79ac7ac065b198e743c99e8a302c9384b181d
lovrus <lovrus120581@gmail.com>
2024-04-19 22:40:32 +0300

    RandDemo

---
+++ src/chapter_2_21/RandDemo.java
@@ -0,0 +1,30 @@
+package chapter_2_21;
+
+import java.util.Random;
+
+// Демонстрация случайных значений из нормального распределения.
+public class RandDemo {
+    public static void main(String[] args) {
+        Random r = new Random();
+        double val;
+        double sum = 0;
+        int[] bell = new int[10];
+        for (int i = 0; i < 100; i++) {
+            val = r.nextGaussian();
+            sum += val;
+            double t = -2;
+            for (int x = 0; x < 10; x++, t += 0.5)
+                if (val < t) {
+                    bell[x]++;
+                    break;
+                }
+        }
+        System.out.println("Среднее для значений: " + (sum / 100));
+        // Отобразить кривую нормального распределения в повернутом виде.
+        for (int i = 0; i < 10; i++) {
+            for (int x = bell[i]; x > 0; x--)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
