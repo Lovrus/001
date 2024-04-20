325955622f805d5d22ea6dc46db8d37938f8f777
lovrus <lovrus120581@gmail.com>
2024-04-20 22:27:55 +0300

    MyTimerTask

---
+++ src/chapter_2_21/MyTimerTask.java
@@ -0,0 +1,26 @@
+package chapter_2_21;
+// Демонстрация использования классов Timer и TimerTask.
+
+import java.util.*;
+
+public class MyTimerTask extends TimerTask {
+    public void run() {
+        System.out.println("Выполняется задача таймера.");
+    }
+}
+
+class Ttest {
+    public static void main(String[] args) {
+        MyTimerTask myTask = new MyTimerTask();
+        Timer myTimer = new Timer();
+        /* Установить начальную задержку в одну секунду
+        и затем повторять каждые полсекунды.
+         */
+        myTimer.schedule(myTask, 1000, 500);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException exc) {
+        }
+        myTimer.cancel();
+    }
+}
