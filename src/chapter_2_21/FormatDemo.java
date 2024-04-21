b1e6a44bcb5dd7f0a49bd1ae98df881726c66a41
lovrus <lovrus120581@gmail.com>
2024-04-21 11:55:21 +0300

    FormatDemo

---
+++ src/chapter_2_21/FormatDemo.java
@@ -0,0 +1,13 @@
+package chapter_2_21;
+// Очень простой пример использования Formatter.
+
+import java.util.*;
+
+public class FormatDemo {
+    public static void main(String[] args) {
+        Formatter fmt = new Formatter();
+        fmt.format("Форматировать  %s легко: %d %f", "с помощью Java", 10, 98.6);
+        System.out.println(fmt);
+        fmt.close();
+    }
+}
