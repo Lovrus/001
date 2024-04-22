c4f9ed3a1b85e0dd2bde0aaf1b86f1bb7c6d7e65
lovrus <lovrus120581@gmail.com>
2024-04-22 21:53:45 +0300

    FormatDemo3

---
+++ src/chapter_2_21/FormatDemo3.java
@@ -0,0 +1,13 @@
+package chapter_2_21;
+// Демонстрация использования спецификаторов формата $n и %%.
+
+import java.util.*;
+
+public class FormatDemo3 {
+    public static void main(String[] args) {
+        Formatter ftm = new Formatter();
+        ftm.format("Копирование файла%nПередача завершена на %d%%", 88);
+        System.out.println(ftm);
+        ftm.close();
+    }
+}
