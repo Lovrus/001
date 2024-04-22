9caa9649c998f259d6f4a38d17d2b0f127702fac
lovrus <lovrus120581@gmail.com>
2024-04-22 22:06:47 +0300

    FormatDemo4

---
+++ src/chapter_2_21/FormatDemo4.java
@@ -0,0 +1,15 @@
+package chapter_2_21;
+// Демонстрация использования спецификатора минимальной ширины поля.
+
+import java.util.*;
+
+public class FormatDemo4 {
+    public static void main(String[] args) {
+        Formatter fmt = new Formatter();
+        fmt.format("|%f|%n|%12f|%n|%012|",
+                10.12345, 10.12345, 10.12345);
+        System.out.println(fmt);
+        fmt.close();
+    }
+}
+
