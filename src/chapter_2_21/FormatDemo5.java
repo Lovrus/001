02b4d7b931082b671212fbb0ece75a6062a52d3f
lovrus <lovrus120581@gmail.com>
2024-04-23 21:00:09 +0300

    FormatDemo5

---
+++ src/chapter_2_21/FormatDemo5.java
@@ -0,0 +1,22 @@
+package chapter_2_21;
+// Демонстрация использования флага пробела.
+
+import java.util.*;
+
+public class FormatDemo5 {
+    public static void main(String[] args) {
+        Formatter fmt = new Formatter();
+        fmt.format("% d", -100);
+        System.out.println(fmt);
+        fmt.close();
+        fmt.format("% d", 100);
+        System.out.println(fmt);
+        fmt.close();
+        fmt.format("% d", -200);
+        System.out.println(fmt);
+        fmt.close();
+        fmt.format("% d", 200);
+        System.out.println(fmt);
+        fmt.close();
+    }
+}
