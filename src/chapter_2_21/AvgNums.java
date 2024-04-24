c510f48410a503d14b82e2c7d8b2bc60c4919bfd
lovrus <lovrus120581@gmail.com>
2024-04-24 22:00:02 +0300

    AvgNums

---
+++ src/chapter_2_21/AvgNums.java
@@ -0,0 +1,30 @@
+package chapter_2_21;
+// Использование класса Scanner для расчета среднего по списку значений.
+
+import java.util.*;
+
+public class AvgNums {
+    public static void main(String[] args) {
+        Scanner conin = new Scanner(System.in);
+        int count = 0;
+        double sum = 0.0;
+        System.out.println("Введите числа для расчета среднего значения" +
+                "(или done для завершения).");
+        // Читать и суммировать числа.
+        while (conin.hasNext()) {
+            if (conin.hasNextDouble()) {
+                sum += conin.nextDouble();
+                count++;
+            } else {
+                String str = conin.next();
+                if (str.equals("done")) break;
+                else {
+                    System.out.println("Ошибка формата данных.");
+                    return;
+                }
+            }
+        }
+        conin.close();
+        System.out.println("Среднее значение равно " + sum / count);
+    }
+}
