92c6c54fdf6c9fd59209ae0434ff23a7fefb1a1d
lovrus <lovrus120581@gmail.com>
2024-04-25 22:32:34 +0300

    AvgFile

---
+++ src/chapter_2_21/AvgFile.java
@@ -0,0 +1,34 @@
+package chapter_2_21;
+// Использование класса Scanner для расчета среднего по значениям в файле.
+
+import java.util.*;
+import java.io.*;
+
+public class AvgFile {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        double sum = 0.0;
+        // Записать выходные данные в файл.
+        FileWriter fout = new FileWriter("test.txt");
+        fout.write("2 3.4 5 6 7.4 9.1 10.5 done");
+        fout.close();
+        FileReader fin = new FileReader("Test.txt");
+        Scanner src = new Scanner(fin);
+        // Читать и суммировать числа.
+        while (src.hasNext()) {
+            if (src.hasNextDouble()) {
+                sum += src.nextDouble();
+                count++;
+            } else {
+                String str = src.next();
+                if (str.equals("done")) break;
+                else {
+                    System.out.println("Ошибка формата файла.");
+                    return;
+                }
+            }
+        }
+        src.close();
+        System.out.println("Среднее значение равно " + sum / count);
+    }
+}
