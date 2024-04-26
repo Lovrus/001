0a44de375174cc87655d2cc1af23128436af4074
lovrus <lovrus120581@gmail.com>
2024-04-26 21:04:39 +0300

    SetDelimiters

---
+++ src/chapter_2_21/SetDelimiters.java
@@ -0,0 +1,38 @@
+package chapter_2_21;
+// Использование класса Scanner для расчета среднего по списку значений,
+// разделенных запятыми.
+
+import java.util.*;
+import java.io.*;
+
+public class SetDelimiters {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        double sum = 0.0;
+        // Записать выходные данные в файл.
+        FileWriter fout = new FileWriter("Test.txt");
+        // Сохранить значения в списке, разделяя их запятыми.
+        fout.write("2, 3.4,      5, 6, 7.4, 9.1, 10.5, done");
+        fout.close();
+        FileReader fin = new FileReader("Test.txt");
+        Scanner src = new Scanner(fin);
+        // Установить пробел и запятую в качестве разделителей.
+        src.useDelimiter(", *");
+        // Читать и суммировать числа.
+        while (src.hasNext()) {
+            if (src.hasNextDouble()) {
+                sum += src.nextDouble();
+                count++;
+            } else {
+                String str = src.next();
+                if (str.equals("done")) break;
+                else {
+                    System.out.println("Ошибка формата файла.");
+                    return;
+                }
+            }
+        }
+        src.close();
+        System.out.println("Среднее значение равно  " + sum / count);
+    }
+}
