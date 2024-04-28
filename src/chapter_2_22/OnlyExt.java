d94dc2b16028fb69ca22a0427158b9fa9233207e
lovrus <lovrus120581@gmail.com>
2024-04-28 19:48:41 +0300

    OnlyExt

---
+++ src/chapter_2_22/OnlyExt.java
@@ -0,0 +1,15 @@
+package chapter_2_22;
+
+import java.io.*;
+
+public class OnlyExt implements FilenameFilter {
+    String ext;
+
+    public OnlyExt(String ext) {
+        this.ext = "." + ext;
+    }
+
+    public boolean accept(File dir, String name) {
+        return name.endsWith(ext);
+    }
+}
