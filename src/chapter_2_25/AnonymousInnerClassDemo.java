2112a7ec75f7568024c3a44b5e8198d418875a1f
lovrus <lovrus120581@gmail.com>
2024-05-25 10:21:04 +0300

    AnonymousInnerClassDemo

---
+++ src/chapter_2_25/AnonymousInnerClassDemo.java
@@ -0,0 +1,38 @@
+package chapter_2_25;
+// Демонстрация использования анонимных внутренних классов.
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class AnonymousInnerClassDemo extends Frame {
+    String msg = "";
+
+    public AnonymousInnerClassDemo() {
+        // Анонимный внутренний класс для обработки событий нажатия кнопки мыши.
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                msg = "MousePressed";
+                // Кнопка мыши нажата
+                repaint();
+            }
+        });
+        // Анонимный внутренний класс для обработки событий закрытия окна.
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(msg, 20, 80);
+    }
+
+    public static void main(String[] args) {
+        AnonymousInnerClassDemo appwin =
+                new AnonymousInnerClassDemo();
+        appwin.setSize(new Dimension(200, 150));
+        appwin.setTitle("AnonymousInnerClassDemo");
+        appwin.setVisible(true);
+    }
+}
