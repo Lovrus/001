f2cd78a28dbaa87216a1d8883a99351439af5d50
lovrus <lovrus120581@gmail.com>
2024-06-03 22:24:59 +0300

    LabelDemo

---
+++ src/chapter_2_27/LabelDemo.java
@@ -0,0 +1,31 @@
+package chapter_2_27;
+// Демонстрация использования меток.
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class LabelDemo extends Frame {
+    public LabelDemo() {
+        // Использовать поточную компоновку.
+        setLayout(new FlowLayout());
+        Label one = new Label("One");
+        Label two = new Label("Two");
+        Label three = new Label("Three");
+        // Добавить метки во фрейм.
+        add(one);
+        add(two);
+        add(three);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        LabelDemo appwin = new LabelDemo();
+        appwin.setSize(new Dimension(300, 100));
+        appwin.setTitle("LabelDemo");
+        appwin.setVisible(true);
+    }
+}
