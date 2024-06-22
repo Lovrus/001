55bbe89141537b9ffcfa07c18b06afe5de0112a9
lovrus <lovrus120581@gmail.com>
2024-06-22 23:43:21 +0300

    MeпuDemo_3

--- src/chapter_2_27/MeпuDemo.java
+++ src/chapter_2_27/MeпuDemo.java
@@ -3,9 +3,11 @@ package chapter_2_27;
 
 import java.awt.*;
 import java.awt.event.*;
+
 public class MeпuDemo extends Frame {
     String msg = "";
     CheckboxMenuItem debug, test;
+
     public MeпuDemo() {
         // Создать панель меню и добавить ее во фрейм.
         MenuBar mbar = new MenuBar();
@@ -24,7 +26,7 @@ public class MeпuDemo extends Frame {
         MenuItem item6, item7, item8, item9;
         edit.add(item6 = new MenuItem("Cut")); // Вырезать
         edit.add(item7 = new MenuItem("Copy")); // Скопировать
-        edit.add(item8 = new  MenuItem("Paste")); // Вставить.
+        edit.add(item8 = new MenuItem("Paste")); // Вставить.
         edit.add(item9 = new MenuItem("-"));
 
         Menu sub = new Menu("Special"); // Сервис
@@ -56,6 +58,60 @@ public class MeпuDemo extends Frame {
         item10.addActionListener(handler);
         item11.addActionListener(handler);
         item12.addActionListener(handler);
+        debug.addItemListener(handler);
+        test.addItemListener(handler);
+// Использовать лямбда-выражение для обработки выбора пункта Quit.
+        item5.addActionListener((ae) -> System.exit(0));
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
 
+    public void paint(Graphics g) {
+        g.drawString(msg, 10, 220);
+        if (debug.getState())
+            g.drawString("Debug is on.", 10, 240); // Пункт Debug отмечен.
+        else
+            g.drawString("Debug is off.", 10, 240); // Пункт Debug не отмечен.
+        if (test.getState())
+            g.drawString("Testing is on.", 10, 260); // Пункт Testing отмечен.
+        else
+            g.drawString("Testing is off.", 10, 260); // Пункт Testing не отмечен
+    }
+
+    public static void main(String[] args) {
+        MeпuDemo appwin = new MeпuDemo();
+        appwin.setSize(new Dimension(250, 300));
+        appwin.setTitle("MeпuDemo");
+        appwin.setVisible(true);
+    }
+
+    // Внутренний классдля обработки событий действий и элементов в меню.
+    class MyMenuHandler implements ActionListener, ItemListener {
+        // Обработать события действий.
+        public void actionPerformed(ActionEvent ae) {
+            msg = "You selected "; // Выбран пункт меню...
+            String arg = ae.getActionCommand();
+            if (arg.equals("New...")) msg += "New.";
+            else if (arg.equals("Open...")) msg += "Open.";
+            else if (arg.equals("Close")) msg += "Close.";
+            else if (arg.equals("Edit")) msg += "Edit.";
+            else if (arg.equals("Cut")) msg += "Cut.";
+            else if (arg.equals("Copy")) msg += "Copy.";
+            else if (arg.equals("Paste")) msg += "Paste.";
+            else if (arg.equals("First")) msg += "First.";
+            else if (arg.equals("Second")) msg += "Second.";
+            else if (arg.equals("Third")) msg += "Third.";
+            else if (arg.equals("Debug")) msg += "Debug.";
+            else if (arg.equals("Testing")) msg += "Testing.";
+            repaint();
+        }
+
+        // Обработать события элементов.
+        public void itemStateChanged(ItemEvent ie) {
+            repaint();
+        }
     }
 }
