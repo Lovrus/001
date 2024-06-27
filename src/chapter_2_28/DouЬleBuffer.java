9624fc95389ec73262393a84acd91623bc9cf85f
lovrus <lovrus120581@gmail.com>
2024-06-27 21:53:51 +0300

    DouЬleBuffer

---
+++ src/chapter_2_28/DouЬleBuffer.java
@@ -0,0 +1,29 @@
+package chapter_2_28;
+// Демонстрация использования внеэкранного буфера.
+import java.awt.*;
+import java.awt.event.*;
+public class DouЬleBuffer extends Frame {
+    int gap = 3;
+    int mx, my;
+    boolean flicker = true;
+    Image buffer = null;
+    int w = 400, h = 400;
+
+    public DouЬleBuffer() {
+        addMouseListener(new MouseMotionAdapter() {
+            public void mouseDragger(MouseEvent me) {
+                mx = me.getX();
+                my = me.getY();
+                flicker = false;
+                repaint();
+            }
+            public void mouseMoved (MouseEvent me) {
+                mx = me.getX();
+                my = me.getY();
+                flicker = true;
+                repaint();
+            }
+        });
+
+    }
+}
