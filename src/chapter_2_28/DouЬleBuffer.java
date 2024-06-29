3b005a70fc8ce7c4715867d8a09d04da651d555c
lovrus <lovrus120581@gmail.com>
2024-06-29 23:19:52 +0300

    DouЬleBuffer_3

--- src/chapter_2_28/DouЬleBuffer.java
+++ src/chapter_2_28/DouЬleBuffer.java
@@ -1,7 +1,9 @@
 package chapter_2_28;
 // Демонстрация использования внеэкранного буфера.
+
 import java.awt.*;
 import java.awt.event.*;
+
 public class DouЬleBuffer extends Frame {
     int gap = 3;
     int mx, my;
@@ -17,18 +19,34 @@ public class DouЬleBuffer extends Frame {
                 flicker = false;
                 repaint();
             }
-            public void mouseMoved (MouseEvent me) {
+
+            public void mouseMoved(MouseEvent me) {
                 mx = me.getX();
                 my = me.getY();
                 flicker = true;
                 repaint();
             }
         });
-addWindowListener(new WindowAdapter() {
-    public void windowsClosing(WindowEvent we) {
-        System.exit(0);
-    }
-});
+        addWindowListener(new WindowAdapter() {
+            public void windowsClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
     }
 
+    public void paint(Graphics g) {
+        Graphics screengc = null;
+        if (!flicker) {
+            screengc = g;
+            g = buffer.getGraphics();
+        }
+        g.getColor(Color.blue);
+        g.fillRect(0,0,w,h);
+        g.getColor(Color.red);
+        for (int i = 0;i<w; i+= gap)
+            g.drawLine(i, w-i, h);
+        for (int i = 0; i<h; i+=gap);
+        g.drawLine(0,i,w, h-i);
+
+    }
 }
