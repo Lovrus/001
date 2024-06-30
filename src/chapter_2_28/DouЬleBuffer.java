7840e975dbb1a0cb5a5b0710d15289c1ad0ce146
lovrus <lovrus120581@gmail.com>
2024-06-30 21:49:06 +0300

    DouЬleBuffer_4

--- src/chapter_2_28/DouЬleBuffer.java
+++ src/chapter_2_28/DouЬleBuffer.java
@@ -12,7 +12,7 @@ public class DouЬleBuffer extends Frame {
     int w = 400, h = 400;
 
     public DouЬleBuffer() {
-        addMouseListener(new MouseMotionAdapter() {
+        addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseDragger(MouseEvent me) {
                 mx = me.getX();
                 my = me.getY();
@@ -40,13 +40,33 @@ public class DouЬleBuffer extends Frame {
             screengc = g;
             g = buffer.getGraphics();
         }
-        g.getColor(Color.blue);
-        g.fillRect(0,0,w,h);
-        g.getColor(Color.red);
-        for (int i = 0;i<w; i+= gap)
-            g.drawLine(i, w-i, h);
-        for (int i = 0; i<h; i+=gap);
-        g.drawLine(0,i,w, h-i);
+        g.setColor(Color.blue);
+        g.fillRect(0, 0, w, h);
+        g.setColor(Color.red);
+        for (int i = 0; i < w; i += gap)
+            g.drawLine(i, 0, w - i, h);
+        for (int i = 0; i < h; i += gap)
+            g.drawLine(0, i, w, h - i);
+        g.setColor(Color.black);
+        g.drawString("Press mouse button to double buffer", 10, h / 2);
+// Нажмите кнопу мыши, чтобы удвоить буфер
+        g.setColor(Color.yellow);
+        g.fillOval(mx - gap, my - gap, gap * 2 + 1, gap * 2 + 1);
+        if (!flicker) {
+            screengc.drawImage(buffer, 0, 0, null);
+        }
+    }
 
+    public void update(Graphics g) {
+        paint(g);
+    }
+
+    public static void main(String[] args) {
+        DouЬleBuffer appwin = new DouЬleBuffer();
+        appwin.setSize(new Dimension(400, 400));
+        appwin.setTitle("DouЬleBuffer");
+        appwin.setVisible(true);
+        // Создать внеэкранный буфер.
+        appwin.buffer = appwin.createImage(appwin.w, appwin.h);
     }
 }
