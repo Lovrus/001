5a45993f50d37d25be702bf62a7b35f1c41cb1f6
lovrus <lovrus120581@gmail.com>
2024-07-01 21:53:43 +0300

    MemoryImageGenerator

---
+++ src/chapter_2_28/MemoryImageGenerator.java
@@ -0,0 +1,12 @@
+package chapter_2_28;
+//Создание изображения в памяти.
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+public class MemoryImageGenerator extends Frame {
+    Image img;
+    int w = 512;
+    int h = 512;
+
+
+}
