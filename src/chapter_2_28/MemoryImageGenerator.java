1ab6f51eb57b657b987636167354e40d8c25c69a
lovrus <lovrus120581@gmail.com>
2024-07-02 21:48:36 +0300

    MemoryImageGenerator2

--- src/chapter_2_28/MemoryImageGenerator.java
+++ src/chapter_2_28/MemoryImageGenerator.java
@@ -8,5 +8,18 @@ public class MemoryImageGenerator extends Frame {
     int w = 512;
     int h = 512;
 
+public  MemoryImageGenerator() {
+    img[] pixels = new  img[w*h];
+    int i = 0;
+    for (int y =0; y<h; y++) {
+        for (int x = 0; x<w; x++) {
+            int r = (x^y)&0xff;
+            int g = (x*2^y*2)&0xff;
+            int b = (x*4^y*4) &0xff;
+            pixels[i++] = (255<<24) | (r<<16) | (g << 8) | b;
 
+
+        }
+    }
+}
 }
