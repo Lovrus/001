82570b72408b99d8ed39dd363099090913f41a49
lovrus <lovrus120581@gmail.com>
2024-07-11 22:26:11 +0300

    HistoGrab4

--- src/chapter_2_28/HistoGrab.java
+++ src/chapter_2_28/HistoGrab.java
@@ -1,5 +1,6 @@
 package chapter_2_28;
 // Демонстрация использования PixelGraber.
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
@@ -14,24 +15,32 @@ public class HistoGrab extends Frame {
     int[] hist = new int[256];
     int max_hist = 0;
     Insets ins;
-    public HistoGrab () {
-try {
-    File imageFile = new File("Lilies.jpg");
-    // Загрузить изображение.
-    img = ImageIO.read(imageFile);
-    iw = img.getWidth(null);
-    ih = img.getHeight(null);
-    pixels = new int[iw*ih];
-    PixelGrabber pg = new PixelGrabber(img, 0,0, iw, ih,
-            pixels, 0,iw);
-    pg.grabPixels();
-} catch (InterruptedException e) {
-    System.out.println("Interrupted");
-    return;
-} catch (IOException exc) {
-    System.out.println("Не удалось загрузить файл изображения.");
-    System.exit(0);
-}
 
+    public HistoGrab() {
+        try {
+            File imageFile = new File("Lilies.jpg");
+            // Загрузить изображение.
+            img = ImageIO.read(imageFile);
+            iw = img.getWidth(null);
+            ih = img.getHeight(null);
+            pixels = new int[iw * ih];
+            PixelGrabber pg = new PixelGrabber(img, 0, 0, iw, ih,
+                    pixels, 0, iw);
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+            return;
+        } catch (IOException exc) {
+            System.out.println("Не удалось загрузить файл изображения.");
+            System.exit(0);
+        }
+        for (int i = 0; i < iw * ih; i++) {
+            int p = pixels[i];
+            int r = 0xff & (p >> 16);
+            int g = 0xff & (p >> 8);
+            int b = 0xff & (p);
+            int y = (int) (.33 * r + .56 * g + .11 * b);
+            hist[y]++;
+        }
     }
 }
