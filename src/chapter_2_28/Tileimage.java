bf9737d0181a38788476e23cfaf4fbdc8db9c76d
lovrus <lovrus120581@gmail.com>
2024-07-17 23:33:26 +0300

    Tileimage

---
+++ src/chapter_2_28/Tileimage.java
@@ -0,0 +1,9 @@
+package chapter_2_28;
+// Демонстрация использования CropImageFilter.
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.imageio.*;
+import java.io.*;
+public class Tileimage extends Frame {
+}
