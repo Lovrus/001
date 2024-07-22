0109e1a6152f4f4a512f15d54eb0ea0cffa43fd0
lovrus <lovrus120581@gmail.com>
2024-07-22 23:20:45 +0300

    ImageFilterDemo

---
+++ src/chapter_2_28/ImageFilterDemo.java
@@ -0,0 +1,9 @@
+package chapter_2_28;
+// Демонстрация использования фильтров изображений.
+import java.awt.*;
+import java.awt.event.*;
+import javax.imageio.*;
+import java.io.*;
+import java.lang.reflect.*;
+public class ImageFilterDemo {
+}
