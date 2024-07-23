57418637e5def354656a45d5bfdf44277ba35d19
lovrus <lovrus120581@gmail.com>
2024-07-23 23:19:18 +0300

    ImageFilterDemo2

--- src/chapter_2_28/ImageFilterDemo.java
+++ src/chapter_2_28/ImageFilterDemo.java
@@ -5,5 +5,15 @@ import java.awt.event.*;
 import javax.imageio.*;
 import java.io.*;
 import java.lang.reflect.*;
-public class ImageFilterDemo {
+public class ImageFilterDemo extends Frame implements ActionListener {
+    Image img;
+    PlugInFilter pif;
+    Image fimg;
+    Image curImg;
+    LoaderImage lim;
+    label lab;
+    Button reset;
+    // Имена фильтров.
+    String[] filters = {"Grayscale", "Invert", "Contrast", "Blur", "Sharpen"};
+
 }
