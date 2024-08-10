3369421d743b78ce7840fbd2702694e0ed78e5bb
lovrus <lovrus120581@gmail.com>
2024-08-10 14:36:40 +0300

    Convolver2

--- src/chapter_2_28/Convolver.java
+++ src/chapter_2_28/Convolver.java
@@ -1,12 +1,19 @@
 package chapter_2_28;
 // Фильтр свертки.
+import DomZad.ImageReader;
+
 import java.awt.*;
 import java.awt.image.*;
 
-public class Convolver implements ImageConsumer, PlugInFilter {
+abstract class Convolver implements ImageConsumer, PlugInFilter {
     int width, height;
     int[] imgpixels, newimgpixels;
     boolean imageReady = false;
-    abstract void convolve; // Здесь находится фильтр...
-
+    abstract void convolve(); // Здесь находится фильтр...
+public Image filter(Frame f, Image in) {
+    ImageReady = false;
+    in.getSource().startProduction(this);
+    waitForImage();
+    newimgpixels = new int[width*height];
+}
 }
