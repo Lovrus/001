26dca73497d9244d32ef85239c4d28962b97ee1a
lovrus <lovrus120581@gmail.com>
2024-08-11 11:40:30 +0300

    Convolver3

--- src/chapter_2_28/Convolver.java
+++ src/chapter_2_28/Convolver.java
@@ -15,5 +15,29 @@ public Image filter(Frame f, Image in) {
     in.getSource().startProduction(this);
     waitForImage();
     newimgpixels = new int[width*height];
+    try {
+        convolve();
+    } catch (Exception e) {
+        System.out.println("Отказ Convolver: " + e);
+        e.printStackTrace();
+    }
+    return f.createImage(
+            new MemoryImageSource(width, height, newimgpixels, 0, width));
 }
+synchronized void waitForImage() {
+    try {
+        while (!imageReady)
+            wait();;
+    } catch (Exception e) {
+        System.out.println("Interrupted"); // Прервано
+    }
+}
+public void setProperties(java.util.Hashtable<?,?> dummy) { }
+    public void setColorModel(ColorModel dummy) { }
+    public void setHints(int dummy) { }
+    public synchronized void imageComlete(int dummy) {
+    imageReady = true;
+    notifyAll();
+    }
+
 }
