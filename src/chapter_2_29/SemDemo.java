0535ba23f37cbbbf6429df5cb346e05351fe054a
lovrus <lovrus120581@gmail.com>
2024-08-18 22:45:51 +0300

    SemDemo2

--- src/chapter_2_29/SemDemo.java
+++ src/chapter_2_29/SemDemo.java
@@ -16,3 +16,22 @@ class Shared {
     static int count = 0;
 }
 // Поток выполнения, который инкементирует счетчик (переменную count).
+class IncThread implements Runnable {
+    String name;
+    Semaphore sem;
+    IncThread (Semaphore s, String n) {
+        sem = s;
+        name = n;
+    }
+    public void run() {
+        System.out.println("Запуск " + name);
+        try {
+            // Для начала получить разрешение.
+            System.out.println(name + "ожидает разрешения.");
+            sem.acquire();
+            System.out.println(name + " получил разрешение.");
+            // Теперь получить доступ к общему ресурсу.
+
+        }
+    }
+}
