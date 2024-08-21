8fde99c82286cade291d796a64c31fcc3e52d7b0
lovrus <lovrus120581@gmail.com>
2024-08-21 21:31:06 +0300

    Q2

--- src/chapter_2_29/Q.java
+++ src/chapter_2_29/Q.java
@@ -19,4 +19,41 @@ public class Q {
         System.out.println("Получено: " + n);
         semProd.release();
     }
+
+    public void put(int i) {
+    }
+}
+
+class Producer implements Runnable {
+    Q q;
+
+    Producer(Q q) {
+        this.q = q;
+    }
+
+    public void run() {
+        for (int i = 0; i < 20; i++)
+            q.put(i);
+    }
+}
+
+class Consumer implements Runnable {
+    Q q;
+
+    Consumer(Q q) {
+        this.q = q;
+    }
+
+    public void run() {
+        for (int i = 0; i < 20; i++)
+            q.get();
+    }
+}
+
+class ProdCon {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Thread(new Consumer(q), "Consumer").start();
+        new Thread(new Producer(q), "Producer").start();
+    }
 }
