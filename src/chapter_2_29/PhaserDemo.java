ae861c4d0afa776e96ff1b8340024982c4703a73
lovrus <lovrus120581@gmail.com>
2024-08-28 22:49:29 +0300

    PhaserDemo

---
+++ src/chapter_2_29/PhaserDemo.java
@@ -0,0 +1,11 @@
+package chapter_2_29;
+// Пример использования Phaser.
+import java.util.concurrent.*;
+public class PhaserDemo {
+    public static void main(String[] args) {
+        Phaser phsr = new Phaser();
+        int curPhase;
+        System.out.println("Начало");
+
+    }
+}
