838201002a3e9b0b3bc3a41792cb733a18925eca
lovrus <lovrus120581@gmail.com>
2024-08-31 22:35:51 +0300

    MyPhaser2

--- src/chapter_2_29/MyPhaser.java
+++ src/chapter_2_29/MyPhaser.java
@@ -1,10 +1,28 @@
 package chapter_2_29;
 // Пример расширения класса Phaser и переопределения метода onAdvance(),
 // чтобы выполнялось только заданное количество стадий.
+
 import java.util.concurrent.*;
+
 // Расширить класс MyPhaser, чтобы разрешить выполнение
 // только указанного количества стадий.
 public class MyPhaser extends Phaser {
     int numPhases;
 
+    MyPhaser(int parties, int phaseCount) {
+        super(parties);
+        numPhases = phaseCount - 1;
+    }
+
+    // Переопределить метод onAdvance() для выполенения указанного
+    // количества стадий.
+    protected boolean onAdvance(int p, int regParties) {
+        // Этот оператор println() предназначен только для иллюстративных целей
+        // Обычно метод onAdvance() ничего не отображает.
+        System.out.println("Стадия " + p + " завершена.\n");
+        // Если все стадии завершены, тогда возвратить true.
+        if (p == numPhases || regParties == 0) return true;
+        // В противном случае возвратить false.
+        return false;
+    }
 }
