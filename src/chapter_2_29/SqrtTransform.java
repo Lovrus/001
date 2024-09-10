a281aa11b40f06de7ac6b8337d32cb724ef54878
lovrus <lovrus120581@gmail.com>
2024-09-10 23:41:10 +0300

    SqrtTransform

---
+++ src/chapter_2_29/SqrtTransform.java
@@ -0,0 +1,9 @@
+package chapter_2_29;
+// Простой пример применения базовой стретегии "разделяй и властвуй".
+// В этом случае используется RecursiveAction.
+import java.util.concurrent.*;
+import java.util.*;
+// Задача ForkJoinTask, которая (через RecursiveAction) трансформирует
+// элементы массива значений double в их квадратные корни.
+public class SqrtTransform extends RecursiveAction {
+}
