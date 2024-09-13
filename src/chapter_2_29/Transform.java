d22207d6a1c27fd99a97a0517a5af84cff7dfe37
lovrus <lovrus120581@gmail.com>
2024-09-13 22:47:57 +0300

    Transform

---
+++ src/chapter_2_29/Transform.java
@@ -0,0 +1,8 @@
+package chapter_2_29;
+// Простая программа, которая позволяет эксперементировать с эффектами
+// от изменения порогового значения и уровня параллелизма задачи ForkJoinTask
+
+import java.util.concurrent.*;
+
+public class Transform {
+}
