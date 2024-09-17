2259d7aabf2f1ef64e7d64d926f44a175db2e532
lovrus <lovrus120581@gmail.com>
2024-09-17 21:34:28 +0300

    Sum2

--- src/chapter_2_29/Sum.java
+++ src/chapter_2_29/Sum.java
@@ -1,6 +1,14 @@
 package chapter_2_29;
 // Простой пример использования RecursiveTask<V>.
 import java.util.concurrent.*;
-// Задача RecurrsiveTask
-public class Sum {
+// Задача RecurrsiveTask, которая вычисляет сумму значений
+// в массиве типа double.
+public class Sum extends RecursiveTask<Double> {
+    // Порог последовательной обработки.
+    final int seqThresHold = 500;
+    // Массив,  в который будет осуществляться доступ.
+    double[] data;
+    // Определить, какую часть данных обрабатывать.
+    int start, end;
+
 }
