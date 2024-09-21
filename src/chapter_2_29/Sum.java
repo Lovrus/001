ed0829a67597db8d512b7f9fd746c15073228676
lovrus <lovrus120581@gmail.com>
2024-09-21 23:54:51 +0300

    Sum6

--- src/chapter_2_29/Sum.java
+++ src/chapter_2_29/Sum.java
@@ -35,6 +35,27 @@ public class Sum extends RecursiveTask<Double> {
             int middle = (start + end)/2;
             // Запустить новые задачи, используя дополнительно разделенные
             // на части данные.
+            Sum subTaskA = new Sum(data, start, middle);
+            Sum subTaskB = new Sum(data, middle, end);
+            // Запустить все задачи с помощью fork().
+            subTaskA.fork();
+            subTaskB.fork();
+            // Ожидать, пока подзадачи возвратят управление,
+            // и объединить результаты.
+            sum = subTaskA.join() + subTaskB.join();
         }
+        // Возвратить окончательную сумму.
+        return  sum;
+    }
+}
+// Демонстрация параллельного выполнения.
+class RecurTaskDemo {
+    public static void main(String[] args) {
+        // Создать пул задач.
+        ForkJoinPool fjp = new ForkJoinPool();
+        double[] nums = new double[5000];
+        // Иницилизировать nums чередующимся положительными
+        // и отрицательными значениями.
+        for ()
     }
 }
