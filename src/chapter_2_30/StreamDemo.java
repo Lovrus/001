1f5fdfbbe21a9106f2b46b8589f36fba88e8bc60
lovrus <lovrus120581@gmail.com>
2024-09-24 21:40:15 +0300

    StreamDemo2

--- src/chapter_2_30/StreamDemo.java
+++ src/chapter_2_30/StreamDemo.java
@@ -5,4 +5,16 @@ import java.util.*;
 import java.util.stream.*;
 
 public class StreamDemo {
+    public static void main(String[] args) {
+        // Создать список значений типа Integer.
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(7);
+        myList.add(18);
+        myList.add(10);
+        myList.add(24);
+        myList.add(17);
+        myList.add(5);
+        System.out.println("Исходный список: " + myList);
+
+    }
 }
