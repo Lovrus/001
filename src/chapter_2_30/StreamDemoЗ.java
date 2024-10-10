8a8d44e8cfa1ff0e93972022ed56f8baafebbb3c
lovrus <lovrus120581@gmail.com>
2024-10-10 22:30:47 +0300

    StreamDemo3_2

--- src/chapter_2_30/StreamDemoЗ.java
+++ src/chapter_2_30/StreamDemoЗ.java
@@ -1,7 +1,24 @@
 package chapter_2_30;
 // Демонстрация использования объединяющей функции посредством reduce().
+
 import java.util.*;
 import java.util.stream.*;
 
 public class StreamDemoЗ {
+    public static void main(String[] args) {
+        // Список значений double.
+        ArrayList<Double> myList = new ArrayList<>();
+        myList.add(7.0);
+        myList.add(18.0);
+        myList.add(10.0);
+        myList.add(24.0);
+        myList.add(17.0);
+        myList.add(5.0);
+        double productOfSqrRoots = myList.parallelStream().reduce(
+                1.0,
+                (a, b) -> a * Math.sqrt(b),
+                (a, b) -> a * b
+        );
+        System.out.println("Произведение квадратный корней: " + productOfSqrRoots);
+    }
 }
