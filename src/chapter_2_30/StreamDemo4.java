13897bd1b2f5b6276751916a6fc9de1aa983199a
lovrus <lovrus120581@gmail.com>
2024-10-12 20:10:12 +0300

    StreamDemo4_1

---
+++ src/chapter_2_30/StreamDemo4.java
@@ -0,0 +1,10 @@
+package chapter_2_30;
+// Сопоставление одного потока с другим.
+import java.util.*;
+import java.util.stream.*;
+public class StreamDemo4 {
+    public static void main(String[] args) {
+        // Список значений типа double.
+
+    }
+}
