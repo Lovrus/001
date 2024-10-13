5cbf8da5e3574d989a0b5dd5f0732a1695125af1
lovrus <lovrus120581@gmail.com>
2024-10-13 23:58:21 +0300

    StreamDemo4_2

--- src/chapter_2_30/StreamDemo4.java
+++ src/chapter_2_30/StreamDemo4.java
@@ -1,10 +1,19 @@
 package chapter_2_30;
 // Сопоставление одного потока с другим.
+
 import java.util.*;
 import java.util.stream.*;
+
 public class StreamDemo4 {
     public static void main(String[] args) {
         // Список значений типа double.
+        ArrayList<Double> mylist = new ArrayList<>();
+        mylist.add(7.0);
+        mylist.add(18.0);
+        mylist.add(10.0);
+        mylist.add(24.0);
+        mylist.add(17.0);
+        mylist.add(5.0);
 
     }
 }
