1828cde44001bf94b27deadd2be038b23b97ddd9
lovrus <lovrus120581@gmail.com>
2024-10-15 23:51:12 +0300

    NamePhoneEmail

---
+++ src/chapter_2_30/NamePhoneEmail.java
@@ -0,0 +1,10 @@
+package chapter_2_30;
+// Использование метода map() для создания нового потока,
+// который содержит избранные значения исходного потока.
+
+import java.util.*;
+import java.util.stream.*;
+
+public class NamePhoneEmail {
+
+}
