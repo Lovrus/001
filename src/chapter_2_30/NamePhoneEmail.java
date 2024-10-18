bb03d29bccea4a6f740bd513c7c251a7cc0249c5
lovrus <lovrus120581@gmail.com>
2024-10-18 21:39:54 +0300

    NamePhoneEmail4

--- src/chapter_2_30/NamePhoneEmail.java
+++ src/chapter_2_30/NamePhoneEmail.java
@@ -6,10 +6,13 @@ import java.util.*;
 import java.util.stream.*;
 
 public class NamePhoneEmail {
-String name;
-String phonenum;
-String email;
-NamePhoneEmail (String n, String p, String e) {
+    String name;
+    String phonenum;
+    String email;
 
-}
+    NamePhoneEmail(String n, String p, String e) {
+        name = n;
+        phonenum = p;
+        email = e;
+    }
 }
