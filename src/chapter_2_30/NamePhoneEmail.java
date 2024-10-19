7a85e5dd6058e885cf7324a532a8aff7b25df332
lovrus <lovrus120581@gmail.com>
2024-10-19 23:24:00 +0300

    NamePhoneEmail5

--- src/chapter_2_30/NamePhoneEmail.java
+++ src/chapter_2_30/NamePhoneEmail.java
@@ -16,3 +16,13 @@ public class NamePhoneEmail {
         email = e;
     }
 }
+
+class NamePhone {
+    String name;
+    String phonenum;
+
+    NamePhone(String n, String p) {
+        name = n;
+        phonenum = p;
+    }
+}
