fbefa44d8d1bdfe64bb457fbc339c760e9a12e67
lovrus <lovrus120581@gmail.com>
2024-10-31 21:42:53 +0300

    StreamDemoб_1

---
+++ src/chapter_2_30/StreamDemoб.java
@@ -0,0 +1,12 @@
+package chapter_2_30;
+// Сопоставление Stream с IntStream.
+
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamDemoб {
+    public static void main(String[] args) {
+        // Список значений типа double.
+
+    }
+}
