c1cc4092a4222528e22d79b8b88c1463e710154f
lovrus <lovrus120581@gmail.com>
2024-11-01 22:08:24 +0300

    StreamDemoб_2

--- src/chapter_2_30/StreamDemoб.java
+++ src/chapter_2_30/StreamDemoб.java
@@ -1,12 +1,16 @@
 package chapter_2_30;
 // Сопоставление Stream с IntStream.
-
 import java.util.*;
 import java.util.stream.*;
-
 public class StreamDemoб {
     public static void main(String[] args) {
-        // Список значений типа double.
-
+        // Споисок значений типа double.
+        ArrayList <Double> myList = new ArrayList<>();
+        myList.add(1.1);
+        myList.add(3.6);
+        myList.add(9.2);
+        myList.add(4.7);
+        myList.add(12.1);
+        myList.add(5.0);
     }
 }
