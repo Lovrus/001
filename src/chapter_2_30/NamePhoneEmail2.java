a4c51aca247cddf8c17bc147ffffb8c3f516f0df
lovrus <lovrus120581@gmail.com>
2024-11-10 20:12:16 +0300

    NamePhoneEmail2_5

--- src/chapter_2_30/NamePhoneEmail2.java
+++ src/chapter_2_30/NamePhoneEmail2.java
@@ -30,6 +30,18 @@ class StreamDemo7 {
     public static void main(String[] args) {
 // Список имен, телефонных номеров и адресов электронной почты.
         ArrayList<NamePhoneEmail2> myList = new ArrayList<>();
+        myList.add(new NamePhoneEmail2("Larry", "555 - 5555",
+                "Larry@HerbShildt.com"));
+        myList.add(new NamePhoneEmail2("James", "555 - 4444",
+                "James@HerbShildt.com"));
+        myList.add(new NamePhoneEmail2("Mary", "555 - 3333",
+                "Mary@HerbShildt.com"));
+        // Сопоставить с новым потоком имени и телефонные номера.
+        Stream<NamePhone2> nameAndPhone = myList.stream().map(
+                (a) -> new NamePhone2(a.name, a.phonenum)
+        );
+        // Использовать collect() для создания списка List
+        // с именами и телефонными номерами.
 
     }
 }
