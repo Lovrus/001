1417ae281aeb8227248d26315509cad4cbec67db
lovrus <lovrus120581@gmail.com>
2024-11-13 23:00:08 +0300

    StreamDemo8

---
+++ src/chapter_2_30/StreamDemo8.java
@@ -0,0 +1,9 @@
+package chapter_2_30;
+// Использование итератора с потоком.
+import java.util.*;
+import java.util.stream.*;
+public class StreamDemo8 {
+    public static void main(String[] args) {
+
+    }
+}
