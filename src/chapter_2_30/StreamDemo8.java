0283d606ea41a2204c53da55d63840f73fe57c47
lovrus <lovrus120581@gmail.com>
2024-11-14 20:29:45 +0300

    StreamDemo8_2

--- src/chapter_2_30/StreamDemo8.java
+++ src/chapter_2_30/StreamDemo8.java
@@ -1,9 +1,19 @@
 package chapter_2_30;
 // Использование итератора с потоком.
+
 import java.util.*;
 import java.util.stream.*;
+
 public class StreamDemo8 {
     public static void main(String[] args) {
+// Создать список строк.
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Alpha");
+        myList.add("Beta");
+        myList.add("Gamma");
+        myList.add("Delta");
+        myList.add("Phi");
+        myList.add("Omega");
 
     }
 }
