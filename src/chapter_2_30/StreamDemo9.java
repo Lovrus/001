cefc8474bd14a69da8b01be9da65e03c881701db
lovrus <lovrus120581@gmail.com>
2024-11-17 19:14:13 +0300

    StreamDemo9

---
+++ src/chapter_2_30/StreamDemo9.java
@@ -0,0 +1,23 @@
+package chapter_2_30;
+// Использование сплитератора.
+
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamDemo9 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Alpha");
+        myList.add("Beta");
+        myList.add("Gamma");
+        myList.add("Delta");
+        myList.add("Phi");
+        myList.add("Omega");
+        // Получить поток для ArrayList.
+        Stream<String> myStream = myList.stream();
+        // Получить сплитератор.
+        Spliterator<String> splitItr = myStream.spliterator();
+        // Пройти по элементам в потоке.
+        while (splitItr.tryAdvance((n) -> System.out.println(n))) ;
+    }
+}
