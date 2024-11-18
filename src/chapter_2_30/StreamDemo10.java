fb2138cc976cd5ea89d37e0beac9598a42facc80
lovrus <lovrus120581@gmail.com>
2024-11-18 21:51:05 +0300

    StreamDemo10

---
+++ src/chapter_2_30/StreamDemo10.java
@@ -0,0 +1,10 @@
+package chapter_2_30;
+// Демонстрация использования trySplit().
+import java.util.*;
+import java.util.stream.*;
+public class StreamDemo10 {
+    public static void main(String[] args) {
+        // Создать список строк.
+
+    }
+}
