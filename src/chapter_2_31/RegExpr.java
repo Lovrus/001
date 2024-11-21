4c3a0a00eab1fc46a33f45ebf9307cc592ce2bb2
lovrus <lovrus120581@gmail.com>
2024-11-21 22:10:09 +0300

    RegExpr

---
+++ src/chapter_2_31/RegExpr.java
@@ -0,0 +1,8 @@
+package chapter_2_31;
+// Простая демонстрация работы сопоставления с шаблоном.
+import java.util.regex.*;
+public class RegExpr {
+    public static void main(String[] args) {
+
+    }
+}
