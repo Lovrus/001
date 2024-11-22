c5365bc5c2c876ac29977e4a68b2716f7c79361c
lovrus <lovrus120581@gmail.com>
2024-11-22 22:14:08 +0300

    RegExpr_2

--- src/chapter_2_31/RegExpr.java
+++ src/chapter_2_31/RegExpr.java
@@ -1,8 +1,16 @@
 package chapter_2_31;
 // Простая демонстрация работы сопоставления с шаблоном.
+
 import java.util.regex.*;
+
 public class RegExpr {
     public static void main(String[] args) {
+        Pattern pat;
+        Matcher mat;
+        boolean found;
+        pat = Pattern.compile("Java");
+        mat = pat.matcher("Java");
+        found = mat.matches(); // проверить на совпадение
 
     }
 }
