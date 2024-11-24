d9360bb0304e7ba55f8ffd1fc2b729a3222c6c4f
lovrus <lovrus120581@gmail.com>
2024-11-24 17:46:54 +0300

    RegExpr2

---
+++ src/chapter_2_31/RegExpr2.java
@@ -0,0 +1,14 @@
+package chapter_2_31;
+// Использование метода find() для поиска подпоследовательности.
+
+import java.util.regex.*;
+
+public class RegExpr2 {
+    public static void main(String[] args) {
+        Pattern pat = Pattern.compile("Java");
+        Matcher mat = pat.matcher("Java SE");
+        System.out.println("Поиск Java в Java SE");
+        if (mat.find()) System.out.println("Подпоследовательность найдена");
+        else System.out.println("Совпадений не обнаружено");
+    }
+}
