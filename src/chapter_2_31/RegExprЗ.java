6f266bba8af25282ca9f20bb0019def8d97fd24c
lovrus <lovrus120581@gmail.com>
2024-11-25 21:58:19 +0300

    RegExpr3

---
+++ src/chapter_2_31/RegExprЗ.java
@@ -0,0 +1,8 @@
+package chapter_2_31;
+// Использование метода find() для поиска нескольких вхождений.
+import java.util.regex.*;
+public class RegExprЗ {
+    public static void main(String[] args) {
+
+    }
+}
