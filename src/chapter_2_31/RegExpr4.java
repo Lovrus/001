51bc843b9c7266dcca6505064ab970c27ac9b75b
lovrus <lovrus120581@gmail.com>
2024-11-29 23:46:36 +0300

    RegExpr4_1

--- src/chapter_2_31/RegExpr4.java
+++ src/chapter_2_31/RegExpr4.java
@@ -1,8 +1,15 @@
 package chapter_2_31;
 // Использование квантификатора.
+
 import java.util.regex.*;
+
 public class RegExpr4 {
     public static void main(String[] args) {
-
+        Pattern pat = Pattern.compile("тест");
+        Matcher mat = pat.matcher("тест 1 2 3 тест");
+        while (mat.find()) {
+            System.out.println("тест найдено по индексу " +
+                    mat.start());
+        }
     }
 }
