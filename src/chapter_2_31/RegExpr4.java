7f830f6ada33d9b73545c1053531ae41528ee444
lovrus <lovrus120581@gmail.com>
2024-11-30 23:35:19 +0300

    RegExpr4_2

--- src/chapter_2_31/RegExpr4.java
+++ src/chapter_2_31/RegExpr4.java
@@ -5,11 +5,11 @@ import java.util.regex.*;
 
 public class RegExpr4 {
     public static void main(String[] args) {
-        Pattern pat = Pattern.compile("тест");
-        Matcher mat = pat.matcher("тест 1 2 3 тест");
+        Pattern pat = Pattern.compile("W+");
+        Matcher mat = pat.matcher("W WW WWW");
         while (mat.find()) {
-            System.out.println("тест найдено по индексу " +
-                    mat.start());
+            System.out.println("Совпадение: " +
+                    mat.group());
         }
     }
 }
