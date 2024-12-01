307b32ce07737f2290a5376bb702070ba8430af7
lovrus <lovrus120581@gmail.com>
2024-12-01 23:20:59 +0300

    RegExpr5

---
+++ src/chapter_2_31/RegExpr5.java
@@ -0,0 +1,8 @@
+package chapter_2_31;
+// Использование группового символа и квартификатора.
+import java.util.regex.*;
+public class RegExpr5 {
+    public static void main(String[] args) {
+
+    }
+}
