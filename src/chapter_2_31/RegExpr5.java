819a575fde817edc81d1e7b9aed72bac2f372cac
lovrus <lovrus120581@gmail.com>
2024-12-02 21:54:02 +0300

    RegExpr5_2

--- src/chapter_2_31/RegExpr5.java
+++ src/chapter_2_31/RegExpr5.java
@@ -1,8 +1,13 @@
 package chapter_2_31;
 // Использование группового символа и квартификатора.
+
 import java.util.regex.*;
+
 public class RegExpr5 {
     public static void main(String[] args) {
-
+        Pattern pat = Pattern.compile("e.+d");
+        Matcher mat = pat.matcher("extend cup end table");
+        while (mat.find())
+            System.out.println("Совпадение: " + mat.group());
     }
 }
