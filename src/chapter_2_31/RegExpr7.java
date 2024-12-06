0c9cbdf30c867bfe7fb4c9399456e72367ddaeee
lovrus <lovrus120581@gmail.com>
2024-12-06 21:49:06 +0300

    RegExpr7_2

--- src/chapter_2_31/RegExpr7.java
+++ src/chapter_2_31/RegExpr7.java
@@ -5,6 +5,10 @@ import java.util.regex.*;
 
 public class RegExpr7 {
     public static void main(String[] args) {
-
+// Сопоставить со словами в нижнем регистре.
+        Pattern pat = Pattern.compile("[a-z]+");
+        Matcher mat = pat.matcher("this is a test.");
+        while (mat.find())
+            System.out.println("Соспадение: " + mat.group());
     }
 }
