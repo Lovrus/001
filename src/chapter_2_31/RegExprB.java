4b3148d60a189ada9a4550fd808fba12401c0d8f
lovrus <lovrus120581@gmail.com>
2024-12-08 16:10:09 +0300

    RegExpr8_2

--- src/chapter_2_31/RegExprB.java
+++ src/chapter_2_31/RegExprB.java
@@ -5,6 +5,11 @@ import java.util.regex.*;
 
 public class RegExprB {
     public static void main(String[] args) {
-
+        String str = "Jon Jonathan Frank Ken Todd";
+        Pattern pat = Pattern.compile("Jon.*? ");
+        Matcher mat = pat.matcher(str);
+        System.out.println("Исходная последовательность: " + str);
+        str = mat.replaceAll("Eric ");
+        System.out.println("Модифицированная последовательность: " + str);
     }
 }
