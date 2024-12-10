d3e03dc51b93de2d2654f335fbb9327609c2b736
lovrus <lovrus120581@gmail.com>
2024-12-10 22:30:46 +0300

    ReflectionDemo1

---
+++ src/chapter_2_31/ReflectionDemo1.java
@@ -0,0 +1,10 @@
+package chapter_2_31;
+// Демонстрация применения рефлексии.
+
+import java.lang.reflect.*;
+
+public class ReflectionDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
