64575217238732f99ce19eb9e8d993341deac1b0
lovrus <lovrus120581@gmail.com>
2024-12-11 23:19:10 +0300

    ReflectionDemo2

--- src/chapter_2_31/ReflectionDemo1.java
+++ src/chapter_2_31/ReflectionDemo1.java
@@ -5,6 +5,11 @@ import java.lang.reflect.*;
 
 public class ReflectionDemo1 {
     public static void main(String[] args) {
+        try {
+            Class<?> c = Class.forName("java.awt.Dimension");
+            System.out.println("Конструкторы:");
+            Constructor<?>[] constructors = c.getConstructors();
 
+        }
     }
 }
