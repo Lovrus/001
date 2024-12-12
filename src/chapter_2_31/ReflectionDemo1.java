29bdf36526a42ef1bcab2a08fbf66e5c4d23cb87
lovrus <lovrus120581@gmail.com>
2024-12-12 09:32:20 +0300

    ReflectionDemo2_1

--- src/chapter_2_31/ReflectionDemo1.java
+++ src/chapter_2_31/ReflectionDemo1.java
@@ -9,7 +9,21 @@ public class ReflectionDemo1 {
             Class<?> c = Class.forName("java.awt.Dimension");
             System.out.println("Конструкторы:");
             Constructor<?>[] constructors = c.getConstructors();
-
+            for (int i = 0; i < constructors.length; i++) {
+                System.out.println(" " + constructors[i]);
+            }
+            System.out.println("Поля:");
+            Field[] fields = c.getFields();
+            for (int i = 0; i < fields.length; i++) {
+                System.out.println(" " + fields[i]);
+            }
+            System.out.println("Методы:");
+            Method[] methods = c.getMethods();
+            for (int i = 0; i < methods.length; i++) {
+                System.out.println("" + methods[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Возникло исключение " + e);
         }
     }
 }
