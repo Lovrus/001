ed6f2c8abd0a72919b92f819e5fc61e355ef997b
lovrus <lovrus120581@gmail.com>
2024-12-13 23:38:48 +0300

    ReflectionDemo2_2

---
+++ src/chapter_2_31/ReflectionDemo2.java
@@ -0,0 +1,13 @@
+package chapter_2_31;
+// Отображение открытых методов.
+
+import java.lang.reflect.*;
+
+public class ReflectionDemo2 {
+    public static void main(String[] args) {
+        try {
+            {
+            }
+        }
+
+    }
