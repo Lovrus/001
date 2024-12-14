6cfac44a13eaeebc6bcda6b05538a48a965d691f
lovrus <lovrus120581@gmail.com>
2024-12-14 21:37:35 +0300

    ReflectionDemo2_3

--- src/chapter_2_31/ReflectionDemo2.java
+++ src/chapter_2_31/ReflectionDemo2.java
@@ -6,8 +6,12 @@ import java.lang.reflect.*;
 public class ReflectionDemo2 {
     public static void main(String[] args) {
         try {
-            {
-            }
-        }
+            A a = new A();
+            Class<?> c = a.getClass();
 
+        }
+        {
+        }
     }
+
+}
