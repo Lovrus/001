4a2f541dbaf44c2d75197aede3cb23723c931ed0
lovrus <lovrus120581@gmail.com>
2024-12-17 22:03:00 +0300

    AddClient

---
+++ src/chapter_2_31/AddClient.java
@@ -0,0 +1,20 @@
+package chapter_2_31;
+
+import java.rmi.*;
+
+public class AddClient {
+    public static void main(String[] args) {
+        try {
+            String addServerURL = "rmi://" + args[0] + "/AddServer";
+            AddServerIntf addServerIntf =
+                    (AddServerIntf) Naming.lookup(addServerURL);
+            System.out.println("Первое число: " + args[1]);
+            double d1 = Double.valueOf(args[1]).doubleValue();
+            System.out.println("Второе число: " + args[2]);
+            double d2 = Double.valueOf(args[2]).doubleValue();
+            System.out.println("Сумма: " + addServerIntf.add(d1, d2));
+        } catch (Exception e) {
+            System.out.println("Возникло исключение " + e);
+        }
+    }
+}
