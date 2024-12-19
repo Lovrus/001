4f2e746cb5e013b4675bfa282c82c16d3aa1df5d
lovrus <lovrus120581@gmail.com>
2024-12-19 23:12:17 +0300

    DateFormatDemo

---
+++ src/chapter_2_31/DateFormatDemo.java
@@ -0,0 +1,11 @@
+package chapter_2_31;
+// Демонстрация форматов даты.
+
+import java.text.*;
+import java.util.*;
+
+public class DateFormatDemo {
+    public static void main(String[] args) {
+
+    }
+}
