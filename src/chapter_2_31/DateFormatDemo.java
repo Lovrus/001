97d6569df8780d143d19cac201000299122c4cdc
lovrus <lovrus120581@gmail.com>
2024-12-21 23:24:22 +0300

    DateFormatDemo_2

--- src/chapter_2_31/DateFormatDemo.java
+++ src/chapter_2_31/DateFormatDemo.java
@@ -6,6 +6,15 @@ import java.util.*;
 
 public class DateFormatDemo {
     public static void main(String[] args) {
+        Date date = new Date();
+        DateFormat df;
+        df = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.JAPAN);
+        System.out.println("Япония: " + df.format(date));
 
+        df = DateFormat.getTimeInstance(DateFormat.LONG, Locale.UK);
+        System.out.println("Соединенное Королевство: " + df.format(date));
+
+        df = DateFormat.getTimeInstance(DateFormat.FULL, Locale.CANADA);
+        System.out.println("Канада: " + df.format(date));
     }
 }
