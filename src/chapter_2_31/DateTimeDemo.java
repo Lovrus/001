9bdfff468ba1934478c92cdd92037c645e8d9e84
lovrus <lovrus120581@gmail.com>
2025-01-02 19:25:45 +0300

    DateTimeDemo_2

--- src/chapter_2_31/DateTimeDemo.java
+++ src/chapter_2_31/DateTimeDemo.java
@@ -1,8 +1,13 @@
 package chapter_2_31;
 
+import java.time.*;
+
 // Простой пример использования LocalDate и LocalTime.
 public class DateTimeDemo {
     public static void main(String[] args) {
-
+        LocalDate curDate = LocalDate.now();
+        System.out.println(curDate);
+        LocalTime curTime = LocalTime.now();
+        System.out.println(curTime);
     }
 }
