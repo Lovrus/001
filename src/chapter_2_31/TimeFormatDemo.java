d3873bda5675e21aded0b5778a5e231642857ddb
lovrus <lovrus120581@gmail.com>
2025-01-03 23:55:13 +0300

    TimeFormatDemo

---
+++ src/chapter_2_31/TimeFormatDemo.java
@@ -0,0 +1,11 @@
+package chapter_2_31;
+// Демонстрация форматов времени.
+
+import java.text.*;
+import java.util.*;
+
+public class TimeFormatDemo {
+    public static void main(String[] args) {
+
+    }
+}
