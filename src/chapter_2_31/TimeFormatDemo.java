bb7cd3b4fd236baa0b6047080c576316b8a90adc
lovrus <lovrus120581@gmail.com>
2025-01-04 00:25:55 +0300

    TimeFormatDemo_2

--- src/chapter_2_31/TimeFormatDemo.java
+++ src/chapter_2_31/TimeFormatDemo.java
@@ -6,6 +6,13 @@ import java.util.*;
 
 public class TimeFormatDemo {
     public static void main(String[] args) {
-
+        Date date = new Date();
+        DateFormat df;
+        df = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.JAPAN);
+        System.out.println("Япония: " + df.format(date));
+        df = DateFormat.getTimeInstance(DateFormat.LONG, Locale.UK);
+        System.out.println("Соединенное Королевство: " + df.format(date));
+        df = DateFormat.getTimeInstance(DateFormat.FULL, Locale.CANADA);
+        System.out.println("Канада: " + df.format(date));
     }
 }
