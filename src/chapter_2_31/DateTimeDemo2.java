5c1a21f528d520a01b915ce50fe085dea26a696c
lovrus <lovrus120581@gmail.com>
2025-01-05 21:51:14 +0300

    TimeFormatDemo_3

---
+++ src/chapter_2_31/DateTimeDemo2.java
@@ -0,0 +1,10 @@
+package chapter_2_31;
+
+import java.time.*;
+
+public class DateTimeDemo2 {
+    public static void main(String[] args) {
+        LocalDateTime curDateTime = LocalDateTime.now();
+        System.out.println(curDateTime);
+    }
+}
