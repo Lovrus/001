be83a87a54b3e10f2398332e2d87dfa717ba621c
lovrus <lovrus120581@gmail.com>
2025-01-06 22:15:07 +0300

    TimeFormatDemo_3_1

---
+++ src/chapter_2_31/DateTimeDemo3.java
@@ -0,0 +1,11 @@
+package chapter_2_31;
+// Демонстрация использования DateTimeFormatter.
+
+import java.time.*;
+import java.time.format.*;
+
+public class DateTimeDemo3 {
+    public static void main(String[] args) {
+
+    }
+}
