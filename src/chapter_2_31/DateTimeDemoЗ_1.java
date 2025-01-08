cb097da651e07ba0523da04484a10164e73b7027
lovrus <lovrus120581@gmail.com>
2025-01-08 23:50:44 +0300

    TimeFormatDemo_3_3

---
+++ src/chapter_2_31/DateTimeDemoЗ_1.java
@@ -0,0 +1,12 @@
+package chapter_2_31;
+// Создание специального формата для вывода даты и времени.
+
+import java.time.*;
+import java.time.format.*;
+
+public class DateTimeDemoЗ_1 {
+    public static void main(String[] args) {
+
+    }
+
+}
