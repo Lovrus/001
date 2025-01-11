cdc4ff073217fd8683ed50019141892106c9d1dd
lovrus <lovrus120581@gmail.com>
2025-01-11 21:43:47 +0300

    DateTimeDemo4_2

--- src/chapter_2_31/DateTimeDemo4.java
+++ src/chapter_2_31/DateTimeDemo4.java
@@ -1,9 +1,17 @@
 package chapter_2_31;
 // Разбор строки с датой и временем.
+
 import java.time.*;
 import java.time.format.*;
+
 public class DateTimeDemo4 {
     public static void main(String[] args) {
-
+// Получить объект LocalDateTime путем разбора строки с датой и временем.
+        LocalDateTime curDateTime =
+                LocalDateTime.parse("June 30, 2021 12:01 AM",
+                        DateTimeFormatter.ofPattern("MMMM d',' yyyy hh':'mm a"));
+        // Отобразить результаты разбора.
+        System.out.println(curDateTime.format(
+                DateTimeFormatter.ofPattern("MMMM d',' yyyy h':'mm a")));
     }
 }
