4347c19f56084c4b3e18533e1973dea636cbe616
lovrus <lovrus120581@gmail.com>
2025-01-13 20:43:01 +0300

    EventDemo

---
+++ src/chapter_2_32/EventDemo.java
@@ -0,0 +1,15 @@
+package chapter_2_32;
+// Обработка события в программе Swing.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class EventDemo {
+    JLabel jlab;
+
+    EventDemo() {
+        // Создать контейнер JFrame.
+
+    }
+}
