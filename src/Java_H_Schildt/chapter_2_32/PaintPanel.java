a2818a61ffc42288c4757029eb6f82f50c5a2a88
lovrus <lovrus120581@gmail.com>
2025-01-19 23:54:36 +0300

    PaintPanel

---
+++ src/Java_H_Schildt/chapter_2_32/PaintPanel.java
@@ -0,0 +1,11 @@
+package Java_H_Schildt.chapter_2_32;
+// Рисование линий внутри панели.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+// Этот класс расширяет JPanel. В нем переопределяется метод
+
+public class PaintPanel {
+
+}
