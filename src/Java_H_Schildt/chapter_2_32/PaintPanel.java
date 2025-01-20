e3df45893a9837f9f3a65e6893efa25f82972f5d
lovrus <lovrus120581@gmail.com>
2025-01-20 23:53:10 +0300

    PaintPanel_2

--- src/Java_H_Schildt/chapter_2_32/PaintPanel.java
+++ src/Java_H_Schildt/chapter_2_32/PaintPanel.java
@@ -1,11 +1,20 @@
 package Java_H_Schildt.chapter_2_32;
 // Рисование линий внутри панели.
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
+
 // Этот класс расширяет JPanel. В нем переопределяется метод
+// paintComponent(), чтобы рисовать линии на поверхности панели.
+public class PaintPanel extends JPanel {
+    Insets ins; // Хранить размеры границы панели
+    Random rand; // Используется для генерации случайных чисел
 
-public class PaintPanel {
+    // Создать панель.
+    PaintPanel() {
+        // Разместить рамку вокруг панели.
 
+    }
 }
