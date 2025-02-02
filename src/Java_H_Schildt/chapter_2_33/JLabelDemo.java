a3af73a67b16be7fbaba7487dbe725f916879497
lovrus <lovrus120581@gmail.com>
2025-02-02 11:40:02 +0300

    JLabelDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JLabelDemo.java
@@ -0,0 +1,11 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JLabel.
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JLabelDemo {
+    public JLabelDemo() {
+
+    }
+}
