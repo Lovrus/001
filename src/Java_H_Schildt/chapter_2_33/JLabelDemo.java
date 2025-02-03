d4812ffb81e85a321cc0d62e8ef094ae0e353cc0
lovrus <lovrus120581@gmail.com>
2025-02-03 20:37:46 +0300

    JLabelDemo2

--- src/Java_H_Schildt/chapter_2_33/JLabelDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JLabelDemo.java
@@ -6,6 +6,12 @@ import javax.swing.*;
 
 public class JLabelDemo {
     public JLabelDemo() {
+// Настроить JFrame.
+        JFrame jfrm = new JFrame("JlabelDemo");
+        jfrm.setLayout(new FlowLayout());
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setSize(260, 210);
+        // Создать значок.
 
     }
 }
