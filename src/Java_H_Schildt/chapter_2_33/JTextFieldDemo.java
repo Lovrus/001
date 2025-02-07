fff3bd5f6f70277e6708e6d352ce355722ce1ba3
lovrus <lovrus120581@gmail.com>
2025-02-07 21:45:52 +0300

    JTextFieldDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JTextFieldDemo.java
@@ -0,0 +1,12 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JTextField.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JTextFieldDemo {
+    public JTextFieldDemo() {
+
+    }
+}
