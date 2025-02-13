ddb4fc45cf4372845dcdbbba01a42332fec93243
lovrus <lovrus120581@gmail.com>
2025-02-13 21:50:34 +0300

    JButtonDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JButtonDemo.java
@@ -0,0 +1,11 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования кнопки JButton на основе значка.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JButtonDemo implements ActionListener {
+    JLabel jlab;
+
+}
