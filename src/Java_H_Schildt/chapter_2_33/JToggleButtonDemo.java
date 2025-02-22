38495ae6aabbbb28280abc08c3cb7d600fcc814c
lovrus <lovrus120581@gmail.com>
2025-02-22 23:24:44 +0300

    JToggleButtonDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JToggleButtonDemo.java
@@ -0,0 +1,13 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JToggleButton.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JToggleButtonDemo {
+    public JToggleButtonDemo() {
+        // Настроить JFrame.
+
+    }
+}
