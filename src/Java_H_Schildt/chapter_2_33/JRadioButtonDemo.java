6c29ff9fc120f79a62fd6dc888353c313e911b81
lovrus <lovrus120581@gmail.com>
2025-02-28 21:41:48 +0300

    JRadioButtonDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JRadioButtonDemo.java
@@ -0,0 +1,11 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JRadioButton.
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JRadioButtonDemo implements ActionListener {
+    JLabel jlab;
+
+}
