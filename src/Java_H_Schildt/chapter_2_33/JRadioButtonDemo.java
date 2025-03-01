c8957f5c02e6c917fa8743d5f9b7406b1eda83fc
lovrus <lovrus120581@gmail.com>
2025-03-01 21:21:47 +0300

    JRadioButtonDemo2

--- src/Java_H_Schildt/chapter_2_33/JRadioButtonDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JRadioButtonDemo.java
@@ -8,4 +8,10 @@ import javax.swing.*;
 public class JRadioButtonDemo implements ActionListener {
     JLabel jlab;
 
+    public JRadioButtonDemo() {
+        // Настроить JFrame.
+        JFrame jfrm = new JFrame("JRadioButtonDemo");
+        jfrm.setLayout(new FlowLayout());
+
+    }
 }
