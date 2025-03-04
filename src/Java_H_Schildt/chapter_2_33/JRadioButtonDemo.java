80d5e218e27fa4696f25c3301e0993be67f40274
lovrus <lovrus120581@gmail.com>
2025-03-04 21:04:32 +0300

    JRadioButtonDemo5

--- src/Java_H_Schildt/chapter_2_33/JRadioButtonDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JRadioButtonDemo.java
@@ -24,6 +24,17 @@ public class JRadioButtonDemo implements ActionListener {
         b2.addActionListener(this);
         jfrm.add(b2);
 
+        JRadioButton b3 = new JRadioButton("C");
+        b3.addActionListener(this);
+        jfrm.add(b3);
+
+// Определить группу переключателей.
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(b1);
+        bg.add(b2);
+        bg.add(b3);
+
+        // Создать метку и добавить ее в панель содержимого.
 
     }
 }
