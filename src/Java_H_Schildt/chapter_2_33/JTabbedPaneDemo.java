9bcda38fa4b364fe4483ad981f11c8ace8317ffe
lovrus <lovrus120581@gmail.com>
2025-03-08 21:46:57 +0300

    JTabbedPaneDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JTabbedPaneDemo.java
@@ -0,0 +1,12 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JTabbedPane.
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JTabbedPaneDemo {
+    public JTabbedPaneDemo() {
+        //Настроить JFrame.
+
+    }
+}
