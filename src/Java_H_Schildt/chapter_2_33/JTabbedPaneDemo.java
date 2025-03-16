4cf0ab0931e5199c8776d30a91844efa686de1dd
lovrus <lovrus120581@gmail.com>
2025-03-16 18:13:07 +0300

    JTabbedPaneDemo9

--- src/Java_H_Schildt/chapter_2_33/JTabbedPaneDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JTabbedPaneDemo.java
@@ -50,6 +50,11 @@ class CitiesPanel extends JPanel {
 
 class ColorsPanel extends JPanel {
     public ColorsPanel() {
-
+        JCheckBox cb1 = new JCheckBox("Red");
+        add(cb1);
+        JCheckBox cb2 = new JCheckBox("Green");
+        add(cb2);
+        JCheckBox cb3 = new JCheckBox("Blue");
+        add(cb3);
     }
 }
