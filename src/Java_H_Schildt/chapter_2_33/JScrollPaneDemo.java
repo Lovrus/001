610d97391d0ec2ccf6aa5379c25489e7227958d4
lovrus <lovrus120581@gmail.com>
2025-03-18 20:31:04 +0300

    JScrollPaneDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JScrollPaneDemo.java
@@ -0,0 +1,11 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования области прокрутки.
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JScrollPaneDemo {
+    public JScrollPaneDemo() {
+
+    }
+}
