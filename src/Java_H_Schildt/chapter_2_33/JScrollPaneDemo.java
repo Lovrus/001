29bc4450b2fd49a4f923cc5404ccb26bed3ee4ce
lovrus <lovrus120581@gmail.com>
2025-03-20 21:33:34 +0300

    JScrollPaneDemo3

--- src/Java_H_Schildt/chapter_2_33/JScrollPaneDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JScrollPaneDemo.java
@@ -8,6 +8,12 @@ public class JScrollPaneDemo {
     public JScrollPaneDemo() {
 // Настроить JFrame. Использовать стандартную компоновку BorderLayot.
         JFrame jfrm = new JFrame("JScrollPaneDemo");
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setSize(400, 400);
+// Создать панель и добавить к ней 400 кнопок.
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(20, 20));
+        int b = 0;
 
     }
 }
