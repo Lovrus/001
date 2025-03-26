19466b792fc35ff1328e1bced1d5df4ec4965775
lovrus <lovrus120581@gmail.com>
2025-03-26 20:59:04 +0300

    JListDemo

---
+++ src/Java_H_Schildt/chapter_2_33/JListDemo.java
@@ -0,0 +1,12 @@
+package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JList.
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JListDemo {
+    // Создать список городов.
+
+}
