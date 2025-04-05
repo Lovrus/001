5c32d76272595353e846f32517794c995a7a7b3d
lovrus <lovrus120581@gmail.com>
2025-04-05 23:27:00 +0300

    JComЬoBoxDemo2

--- src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
@@ -1,8 +1,14 @@
 package Java_H_Schildt.chapter_2_33;
 // Демонстрация использования JComЬoBox.
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class JComЬoBoxDemo {
 
+public class JComЬoBoxDemo {
+    String[] timepieces = {"Hourglass", "Analog", "Digital", "Stopwatch"};
+
+    public JComЬoBoxDemo() {
+
+    }
 }
