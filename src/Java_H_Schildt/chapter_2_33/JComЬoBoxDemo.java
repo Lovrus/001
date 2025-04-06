211aa2d79496e6c9c9a66f3e66a531f2a788564c
lovrus <lovrus120581@gmail.com>
2025-04-06 13:49:48 +0300

    JComЬoBoxDemo3

--- src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
@@ -9,6 +9,11 @@ public class JComЬoBoxDemo {
     String[] timepieces = {"Hourglass", "Analog", "Digital", "Stopwatch"};
 
     public JComЬoBoxDemo() {
+// Настроить JFrame.
+        JFrame jfrm = new JFrame("JComЬoBoxDemo");
+        jfrm.setLayout(new FlowLayout());
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setSize(400, 250);
 
     }
 }
