1c094a1b53cc6e4e2c8a8f92df600ab6a9080c16
lovrus <lovrus120581@gmail.com>
2025-04-07 19:58:57 +0300

    JComЬoBoxDemo4

--- src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JComЬoBoxDemo.java
@@ -14,6 +14,10 @@ public class JComЬoBoxDemo {
         jfrm.setLayout(new FlowLayout());
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfrm.setSize(400, 250);
+// Создать комбинированный список и дбавить его в панель содержимого.
+        JComboBox<String> jcb = new JComboBox<String>(timepieces);
+        jfrm.add(jcb);
+        // Создать метку и добавить ее в панель содержимого.
 
     }
 }
