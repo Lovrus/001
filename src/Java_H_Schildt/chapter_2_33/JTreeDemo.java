658e78837cfe613a6dd78bfe2d38c0db0ac8c953
lovrus <lovrus120581@gmail.com>
2025-04-12 09:50:59 +0300

    JTreeDemo2

--- src/Java_H_Schildt/chapter_2_33/JTreeDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JTreeDemo.java
@@ -1,4 +1,18 @@
 package Java_H_Schildt.chapter_2_33;
+// Демонстрация использования JTree.
+
+import java.awt.;
+import javax.swing.event.*;
+import javax.swing.*;
+import javax.swing.tree.*;
 
 public class JTreeDemo {
+    public JTreeDemo() {
+        // Настроить JFrame. Использовать стандартную компоновку BorderLayout.
+        JFrame jfrm = new JFrame("JTreeDemo");
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setSize(200, 250);
+
+    }
+
 }
