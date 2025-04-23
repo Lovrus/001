e69b8cbe1446d607929650c2fb0ed3cb6313a3e0
lovrus <lovrus120581@gmail.com>
2025-04-23 20:35:38 +0300

    JTaЬleDemo3

--- src/Java_H_Schildt/chapter_2_33/JTaЬleDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JTaЬleDemo.java
@@ -1,9 +1,12 @@
 package Java_H_Schildt.chapter_2_33;
 // Демонстрация использования JTable.
+
 import java.awt.*;
 import javax.swing.*;
 
 public class JTaЬleDemo {
-// Инициализировать заголовки колонок.
+    // Инициализировать заголовки колонок.
+    String[] colHeads = {"Name", "Extension", "ID#"};
+// Иницилизировать данные.
 
 }
