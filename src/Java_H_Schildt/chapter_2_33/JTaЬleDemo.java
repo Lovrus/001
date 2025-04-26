9ea559b5026e8c5ada39237782fa7a662c92e395
lovrus <lovrus120581@gmail.com>
2025-04-26 23:07:14 +0300

    JTaЬleDemo5

--- src/Java_H_Schildt/chapter_2_33/JTaЬleDemo.java
+++ src/Java_H_Schildt/chapter_2_33/JTaЬleDemo.java
@@ -7,9 +7,12 @@ import javax.swing.*;
 public class JTaЬleDemo {
     // Инициализировать заголовки колонок.
     String[] colHeads = {"Name", "Extension", "ID#"};
-// Иницилизировать данные.
-Object[] [] data = {
-        { "Gail", "4567", "865"},
+    // Иницилизировать данные.
+    Object[][] data = {
+            {"Gail", "4567", "865"},
+            {"Ken", "7566", "555"},
+            {"Viviane", "5634", "587"},
+            {"Melanie", "7345", "922"},
 
-}
+    }
 }
