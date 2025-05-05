f059faf63695d1f88a3928faf6f585e6be88d40b
lovrus <lovrus120581@gmail.com>
2025-05-05 20:49:29 +0300

    MenuDemo.java

---
+++ src/Java_H_Schildt/chapter_2_34/MenuDemo.java
@@ -0,0 +1,8 @@
+package Java_H_Schildt.chapter_2_34;
+// Демонстрация простого главного меню.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class MenuDemo implements ActionListener {
+
+}
