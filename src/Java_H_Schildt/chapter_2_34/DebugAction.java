ff0466cc938d767478b3a4d3ab6b53943cbbcf70
lovrus <lovrus120581@gmail.com>
2025-06-25 20:58:44 +0300

    DebugAction

---
+++ src/Java_H_Schildt/chapter_2_34/DebugAction.java
@@ -0,0 +1,8 @@
+package Java_H_Schildt.chapter_2_34;
+// Класс представляющий действие для меню Debug и панели инструментов.
+
+import javax.swing.*;
+
+public class DebugAction extends AbstractAction {
+
+}
