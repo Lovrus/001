c5a8d7a33cb09a66f4ff8c2f5d88ccf8a8109d0c
lovrus <lovrus120581@gmail.com>
2025-06-26 22:22:28 +0300

    DebugAction2

--- src/Java_H_Schildt/chapter_2_34/DebugAction.java
+++ src/Java_H_Schildt/chapter_2_34/DebugAction.java
@@ -4,5 +4,8 @@ package Java_H_Schildt.chapter_2_34;
 import javax.swing.*;
 
 public class DebugAction extends AbstractAction {
+public DebugAction (String name, Icon image, int mnem,
+                    int accel, String tTrip) {
 
+}
 }
