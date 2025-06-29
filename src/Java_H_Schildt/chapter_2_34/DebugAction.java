ed91db1119d4eca870e72a2c9d137a0df45f58dc
lovrus <lovrus120581@gmail.com>
2025-06-29 12:13:22 +0300

    DebugAction4

--- src/Java_H_Schildt/chapter_2_34/DebugAction.java
+++ src/Java_H_Schildt/chapter_2_34/DebugAction.java
@@ -2,11 +2,14 @@ package Java_H_Schildt.chapter_2_34;
 // Класс представляющий действие для меню Debug и панели инструментов.
 
 import javax.swing.*;
+import java.awt.event.InputEvent;
 
 public class DebugAction extends AbstractAction {
     public DebugAction(String name, Icon image, int mnem,
                        int accel, String tTrip) {
         super(name, image);
+        putValue(ACCELERATOR_KEY,
+                KeyStroke.getKeyStroke(accel, InputEvent.CTRL_DOWN_MASK));
 
     }
 }
